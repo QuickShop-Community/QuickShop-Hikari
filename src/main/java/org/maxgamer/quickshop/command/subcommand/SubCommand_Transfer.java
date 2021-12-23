3160eb14d06ed63cfeec756a3060e9bab6804399
sandtechnology <a1294790523@hotmail.com>
2021-12-23 13:06:46 +0800

    Fix wrong sender limit in transfer command

--- src/main/java/org/maxgamer/quickshop/command/subcommand/SubCommand_Transfer.java
+++ src/main/java/org/maxgamer/quickshop/command/subcommand/SubCommand_Transfer.java
@@ -20,21 +20,19 @@
 package org.maxgamer.quickshop.command.subcommand;
 
 import org.bukkit.OfflinePlayer;
-import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.maxgamer.quickshop.QuickShop;
 import org.maxgamer.quickshop.api.command.CommandHandler;
 import org.maxgamer.quickshop.api.shop.Shop;
-import org.maxgamer.quickshop.util.MsgUtil;
 import org.maxgamer.quickshop.util.Util;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
-public class SubCommand_Transfer implements CommandHandler<ConsoleCommandSender> {
+public class SubCommand_Transfer implements CommandHandler<Player> {
 
     private final QuickShop plugin;
 
@@ -44,13 +42,8 @@ public class SubCommand_Transfer implements CommandHandler<ConsoleCommandSender>
 
 
     @Override
-    public void onCommand(@NotNull ConsoleCommandSender sender, @NotNull String commandLabel, @NotNull String[] cmdArg) {
+    public void onCommand(@NotNull Player sender, @NotNull String commandLabel, @NotNull String[] cmdArg) {
         if (cmdArg.length == 1) {
-            if (!(sender instanceof Player)) {
-                MsgUtil.sendDirectMessage(sender, "This command can't be run by the console!");
-                return;
-            }
-            //noinspection deprecation
             final OfflinePlayer targetPlayer = plugin.getServer().getOfflinePlayer(cmdArg[0]);
             String targetPlayerName = targetPlayer.getName();
             if (targetPlayerName == null) {
@@ -93,7 +86,7 @@ public class SubCommand_Transfer implements CommandHandler<ConsoleCommandSender>
     }
 
     @Override
-    public @Nullable List<String> onTabComplete(@NotNull ConsoleCommandSender sender, @NotNull String commandLabel, @NotNull String[] cmdArg) {
+    public @Nullable List<String> onTabComplete(@NotNull Player sender, @NotNull String commandLabel, @NotNull String[] cmdArg) {
         return cmdArg.length <= 2 ? Util.getPlayerList() : Collections.emptyList();
     }
 }
