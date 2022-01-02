7405d58adff3c681d75dad77543a07b93c4cf998
Ghost_chu <2908803755@qq.com>
2022-01-02 18:23:29 +0800

    bug fix

--- src/main/java/org/maxgamer/quickshop/QuickShop.java
+++ src/main/java/org/maxgamer/quickshop/QuickShop.java
@@ -782,56 +782,56 @@ public class QuickShop extends JavaPlugin implements QuickShopAPI {
                 if (result.getValue().getResult().ordinal() > CheckResult.WARNING.ordinal()) {
                     joiner.add(String.format("- [%s/%s] %s", result.getValue().getResult().getDisplay(), result.getKey().name(), result.getValue().getResultMessage()));
                 }
+                switch (resultReport.getFinalResult()) {
+                    case DISABLE_PLUGIN:
+                        Bukkit.getPluginManager().disablePlugin(this);
+                        break;
+                    case KILL_SERVER:
+                        getLogger().warning("[Security Risk Detected] QuickShop forcing kill server for security, contact the developer for details.");
+                        System.out.println("[Security Risk Detected] QuickShop forcing kill server for security, contact the developer for details.");
+                        System.err.println("[Security Risk Detected] QuickShop forcing kill server for security, contact the developer for details.");
+                        Paste paste = new Paste(this);
+                        String data = "QuickShop Generated Security Risk Report" + "\n" +
+                                "===============================================\n" +
+                                "Your server has been automatically killed by QuickShop since we detected security risk.\n" +
+                                "You can find more infomation here: https://github.com/PotatoCraft-Studio/QuickShop-Reremake/wiki/QuickShop-halt-my-server\n" +
+                                "Discord Support: https://discord.gg/bfefw2E\n" +
+                                "===============================================\n" +
+                                "Useful information about this halt:\n\n" +
+                                this.securityCacheBuilder.toString();
+                        String url = paste.paste(data);
+                        if (url != null) {
+                            try {
+                                if (java.awt.Desktop.isDesktopSupported()) {
+                                    try {
+                                        java.net.URI uri = java.net.URI.create(url);
+                                        java.awt.Desktop dp = java.awt.Desktop.getDesktop();
+                                        if (dp.isSupported(java.awt.Desktop.Action.BROWSE)) {
+                                            dp.browse(uri);
+                                        }
+                                        System.out.println("More details here: " + url);
+                                    } catch (java.io.IOException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            } catch (Throwable ignored) {
+                            }
+                        }
+                        Runtime.getRuntime().halt(-1);
+                        System.exit(-1);
+                        Bukkit.shutdown();
+                        break;
+                    default:
+                        // Move after halt switch since we found these may throw out exception
+                        if (resultReport.getFinalResult().ordinal() > CheckResult.WARNING.ordinal()) {
+                            setupBootError(new BootError(this.getLogger(), joiner.toString()), true);
+                            //noinspection ConstantConditions
+                            Util.mainThreadRun(() -> getCommand("qs").setTabCompleter(this)); //Disable tab completer
+                        }
+                }
             }
         }
 
-        switch (resultReport.getFinalResult()) {
-            case DISABLE_PLUGIN:
-                Bukkit.getPluginManager().disablePlugin(this);
-                break;
-            case KILL_SERVER:
-                getLogger().warning("[Security Risk Detected] QuickShop forcing kill server for security, contact the developer for details.");
-                System.out.println("[Security Risk Detected] QuickShop forcing kill server for security, contact the developer for details.");
-                System.err.println("[Security Risk Detected] QuickShop forcing kill server for security, contact the developer for details.");
-                Paste paste = new Paste(this);
-                String data = "QuickShop Generated Security Risk Report" + "\n" +
-                        "===============================================\n" +
-                        "Your server has been automatically killed by QuickShop since we detected security risk.\n" +
-                        "You can find more infomation here: https://github.com/PotatoCraft-Studio/QuickShop-Reremake/wiki/QuickShop-halt-my-server\n" +
-                        "Discord Support: https://discord.gg/bfefw2E\n" +
-                        "===============================================\n" +
-                        "Useful information about this halt:\n\n" +
-                        this.securityCacheBuilder.toString();
-                String url = paste.paste(data);
-                if (url != null) {
-                    try {
-                        if (java.awt.Desktop.isDesktopSupported()) {
-                            try {
-                                java.net.URI uri = java.net.URI.create(url);
-                                java.awt.Desktop dp = java.awt.Desktop.getDesktop();
-                                if (dp.isSupported(java.awt.Desktop.Action.BROWSE)) {
-                                    dp.browse(uri);
-                                }
-                                System.out.println("More details here: " + url);
-                            } catch (java.io.IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    } catch (Throwable ignored) {
-                    }
-                }
-                Runtime.getRuntime().halt(-1);
-                System.exit(-1);
-                Bukkit.shutdown();
-                break;
-            default:
-                // Move after halt switch since we found these may throw out exception
-                if (resultReport.getFinalResult().ordinal() > CheckResult.WARNING.ordinal()) {
-                    setupBootError(new BootError(this.getLogger(), joiner.toString()), true);
-                    //noinspection ConstantConditions
-                    Util.mainThreadRun(() -> getCommand("qs").setTabCompleter(this)); //Disable tab completer
-                }
-        }
 
         testing = false;
     }
