6fc72084adacd824727515204a29ce3d24e77a23
sandtechnology <a1294790523@hotmail.com>
2022-01-28 01:35:30 +0800

    Fix a bug in BukkitInventoryWrapperManager

--- src/main/java/org/maxgamer/quickshop/shop/inventory/BukkitInventoryWrapperManager.java
+++ src/main/java/org/maxgamer/quickshop/shop/inventory/BukkitInventoryWrapperManager.java
@@ -1,3 +1,22 @@
+/*
+ * This file is a part of project QuickShop, the name is BukkitInventoryWrapperManager.java
+ *  Copyright (C) PotatoCraft Studio and contributors
+ *
+ *  This program is free software: you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the
+ *  Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *  for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package org.maxgamer.quickshop.shop.inventory;
 
 import lombok.AllArgsConstructor;
@@ -18,7 +37,7 @@ public class BukkitInventoryWrapperManager implements InventoryWrapperManager {
     public @NotNull String mklink(@NotNull InventoryWrapper wrapper) throws IllegalArgumentException {
         if (wrapper.getLocation() != null) {
             Block block = wrapper.getLocation().getBlock();
-            if ((block instanceof Container))
+            if (!(block instanceof Container))
                 throw new IllegalArgumentException("Target reporting it self not a valid Container.");
             String holder = JsonUtil.standard().toJson(new BlockHolder(block.getWorld().getName(), block.getLocation().getBlockX(), block.getLocation().getBlockY(), block.getLocation().getBlockZ()));
             return JsonUtil.standard().toJson(new CommonHolder(HolderType.BLOCK,holder));
