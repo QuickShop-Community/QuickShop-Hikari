07981e1606e58e51653ab0442f64bc720f9d3afb
Ghost_chu <2908803755@qq.com>
2022-02-05 22:19:51 +0800

    merge code

--- src/main/java/org/maxgamer/quickshop/shop/SimpleShopManager.java
+++ src/main/java/org/maxgamer/quickshop/shop/SimpleShopManager.java
@@ -19,6 +19,9 @@
 
 package org.maxgamer.quickshop.shop;
 
+import com.ghostchu.simplereloadlib.ReloadResult;
+import com.ghostchu.simplereloadlib.ReloadStatus;
+import com.ghostchu.simplereloadlib.Reloadable;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.collect.MapMaker;
@@ -56,9 +59,6 @@ import org.maxgamer.quickshop.localization.LocalizedMessagePair;
 import org.maxgamer.quickshop.util.*;
 import org.maxgamer.quickshop.util.economyformatter.EconomyFormatter;
 import org.maxgamer.quickshop.util.holder.Result;
-import org.maxgamer.quickshop.util.reload.ReloadResult;
-import org.maxgamer.quickshop.util.reload.ReloadStatus;
-import org.maxgamer.quickshop.util.reload.Reloadable;
 
 import java.text.DecimalFormat;
 import java.util.*;
@@ -318,7 +318,7 @@ public class SimpleShopManager implements ShopManager, Reloadable {
                     shop.delete(true);
                     plugin.getLogger()
                             .log(Level.WARNING, "Shop create failed, trying to auto fix the database...", e);
-                    boolean backupSuccess = Util.backupDatabase();
+                    boolean backupSuccess = false; // Database backup
                     if (backupSuccess) {
                         plugin.getDatabaseHelper().removeShop(shop);
                     } else {
