1ecf2f99ee7f68b00bad5cae29ba039785aec49c
Ghost_chu <2908803755@qq.com>
2022-02-12 18:00:51 +0800

    re-add compatibility with NoCheatPlus

--- compatibility/nocheatplus/src/main/java/com/ghostchu/quickshop/compatibility/nocheatplus/Main.java
+++ compatibility/nocheatplus/src/main/java/com/ghostchu/quickshop/compatibility/nocheatplus/Main.java
@@ -1,21 +1,18 @@
 package com.ghostchu.quickshop.compatibility.nocheatplus;
 
-import com.google.common.collect.BiMap;
-import com.google.common.collect.HashBiMap;
+import fr.neatmonster.nocheatplus.hooks.NCPExemptionManager;
 import org.bukkit.Bukkit;
-import org.bukkit.block.Block;
-import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.maxgamer.quickshop.api.event.ProtectionCheckStatus;
 import org.maxgamer.quickshop.api.event.ShopProtectionCheckEvent;
 
 public final class Main extends JavaPlugin implements Listener {
-    private final BiMap<Event, Block> biMap = HashBiMap.create();
     @Override
     public void onEnable() {
         // Plugin startup logic
-        Bukkit.getPluginManager().registerEvents(this,this);
+        Bukkit.getPluginManager().registerEvents(this, this);
         getLogger().info("QuickShop Compatibility Module - NoCheatPlus loaded");
     }
 
@@ -23,11 +20,12 @@ public final class Main extends JavaPlugin implements Listener {
     public void onDisable() {
         // Plugin shutdown logic
     }
+
     @EventHandler(ignoreCancelled = true)
-    public void onFakeEventBegin(ShopProtectionCheckEvent event){
-        biMap.put(event.getEvent(),event.getLocation().getBlock());
+    public void onFakeEventBegin(ShopProtectionCheckEvent event) {
+        if (event.getStatus() == ProtectionCheckStatus.BEGIN)
+            NCPExemptionManager.exemptPermanently(event.getPlayer().getUniqueId());
+        else if(event.getStatus() == ProtectionCheckStatus.END)
+            NCPExemptionManager.unexempt(event.getPlayer().getUniqueId());
     }
-
-
-
 }
