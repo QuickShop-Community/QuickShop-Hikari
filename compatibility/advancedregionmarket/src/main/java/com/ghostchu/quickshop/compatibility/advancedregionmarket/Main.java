0288d37b1a846e968579ce4e61e59b1eb35c5e5b
Ghost_chu <2908803755@qq.com>
2022-02-16 17:21:27 +0800

    re-add AdvancedRegionMarket compatibility module

---
+++ compatibility/advancedregionmarket/src/main/java/com/ghostchu/quickshop/compatibility/advancedregionmarket/Main.java
@@ -0,0 +1,99 @@
+/*
+ *  This file is a part of project QuickShop, the name is Main.java
+ *  Copyright (C) Ghost_chu and contributors
+ *
+ *  This program is free software: you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the
+ *  Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *  for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.ghostchu.quickshop.compatibility.advancedregionmarket;
+
+import com.ghostchu.quickshop.api.QuickShopAPI;
+import com.ghostchu.quickshop.api.shop.Shop;
+import net.alex9849.arm.events.RemoveRegionEvent;
+import net.alex9849.arm.events.RestoreRegionEvent;
+import net.alex9849.arm.regions.Region;
+import org.bukkit.Bukkit;
+import org.bukkit.Chunk;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public final class Main extends JavaPlugin implements Listener {
+    private QuickShopAPI api;
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        this.api = (QuickShopAPI) Bukkit.getPluginManager().getPlugin("QuickShop-Hikari");
+        Bukkit.getPluginManager().registerEvents(this,this);
+        getLogger().info("QuickShop Compatibility Module - Clearlag loaded");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    private void handleDeletion(Region region) {
+        Vector minPoint = region.getRegion().getMinPoint();
+        Vector maxPoint = region.getRegion().getMaxPoint();
+        World world = region.getRegionworld();
+        Set<Chunk> chuckLocations = new HashSet<>();
+
+        for (int x = minPoint.getBlockX(); x <= maxPoint.getBlockX() + 16; x += 16) {
+            for (int z = minPoint.getBlockZ(); z <= maxPoint.getBlockZ() + 16; z += 16) {
+                chuckLocations.add(world.getChunkAt(x >> 4, z >> 4));
+            }
+        }
+
+
+        HashMap<Location, Shop> shopMap = new HashMap<>();
+
+        for (Chunk chunk : chuckLocations) {
+            Map<Location, Shop> shopsInChunk = api.getShopManager().getShops(chunk);
+            if (shopsInChunk != null) {
+                shopMap.putAll(shopsInChunk);
+            }
+        }
+        for (Map.Entry<Location, Shop> shopEntry : shopMap.entrySet()) {
+            Location shopLocation = shopEntry.getKey();
+            if (region.getRegion().contains(shopLocation.getBlockX(), shopLocation.getBlockY(), shopLocation.getBlockZ())) {
+                Shop shop = shopEntry.getValue();
+                if (shop != null) {
+                    shop.onUnload();
+                    shop.delete(false);
+                }
+            }
+        }
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onShopNeedDeletion(RestoreRegionEvent event) {
+        handleDeletion(event.getRegion());
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onShopNeedDeletion(RemoveRegionEvent event) {
+        handleDeletion(event.getRegion());
+    }
+}
