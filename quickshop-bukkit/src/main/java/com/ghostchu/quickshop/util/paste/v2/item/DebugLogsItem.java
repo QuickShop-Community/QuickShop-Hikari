bf316e12b8ea5bc58c2b7333b838442ba68c0551
Ghost_chu <2908803755@qq.com>
2022-05-05 15:04:49 +0800

    Add time prefix in debug logs paste

--- quickshop-bukkit/src/main/java/com/ghostchu/quickshop/util/paste/v2/item/DebugLogsItem.java
+++ quickshop-bukkit/src/main/java/com/ghostchu/quickshop/util/paste/v2/item/DebugLogsItem.java
@@ -19,18 +19,27 @@
 
 package com.ghostchu.quickshop.util.paste.v2.item;
 
+import com.ghostchu.quickshop.QuickShop;
+import com.ghostchu.quickshop.util.MsgUtil;
 import com.ghostchu.quickshop.util.logger.Log;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.apache.commons.lang3.StringEscapeUtils;
 import org.jetbrains.annotations.NotNull;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.StringJoiner;
 
 public class DebugLogsItem implements SubPasteItem {
-
+    private SimpleDateFormat format;
 
     public DebugLogsItem() {
-
+        String timeFormat = LegacyComponentSerializer.legacySection().serialize(QuickShop.getInstance().text().of("timeunit.std-time-format").forLocale(MsgUtil.getDefaultGameLanguageCode()));
+        try {
+            format = new SimpleDateFormat(timeFormat);
+        } catch (IllegalArgumentException e) {
+            format = new SimpleDateFormat("HH:mm:ss");
+        }
     }
 
 
@@ -42,14 +51,14 @@ public class DebugLogsItem implements SubPasteItem {
     @NotNull
     private String buildContent() {
         StringJoiner builder = new StringJoiner("\n");
-        List<String> debugLogs = Log.fetchLogs().stream().map(Log.Record::toString).toList();
+        List<String> debugLogs = Log.fetchLogs().stream().map(record -> "[" + format.format(record.getTimestamp()) + "] ").toList();
         List<String> tail = debugLogs.subList(Math.max(debugLogs.size() - 3000, 0), debugLogs.size());
         tail.forEach(builder::add);
         return "<textarea name=\"debuglogs\" style=\"height: 1000px; width: 100%;\">" +
                 StringEscapeUtils.escapeHtml4(builder.toString()) +
                 "</textarea><br />";
     }
-
+   \
 
     @Override
     public @NotNull String genBody() {
