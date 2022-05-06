847a80dd1140bb437c4b0f77786af7c94924badf
Ghost_chu <2908803755@qq.com>
2022-05-06 12:09:35 +0800

    javadoc for new paste

--- quickshop-bukkit/src/main/java/com/ghostchu/quickshop/util/paste/item/PasteItem.java
+++ quickshop-bukkit/src/main/java/com/ghostchu/quickshop/util/paste/item/PasteItem.java
@@ -22,11 +22,11 @@ package com.ghostchu.quickshop.util.paste.item;
 import org.jetbrains.annotations.NotNull;
 
 public interface PasteItem {
+    /**
+     * Render this item to HTML sources
+     *
+     * @return HTML sources
+     */
     @NotNull
     String toHTML();
-
-
-    static class TableBuilder {
-
-    }
 }
--- quickshop-bukkit/src/main/java/com/ghostchu/quickshop/util/paste/item/SubPasteItem.java
+++ quickshop-bukkit/src/main/java/com/ghostchu/quickshop/util/paste/item/SubPasteItem.java
@@ -22,17 +22,37 @@ package com.ghostchu.quickshop.util.paste.item;
 import org.jetbrains.annotations.NotNull;
 
 public interface SubPasteItem extends PasteItem {
+    /**
+     * Generate and render the title part of this item
+     *
+     * @return the rendered title
+     */
     @NotNull
     default String genTitle() {
         return "<h3># " + getTitle() + "</h3>";
     }
 
+    /**
+     * Returns this item's title (plain text), and will render to HTML
+     *
+     * @return the title
+     */
     @NotNull
     String getTitle();
 
+    /**
+     * Generate and render the body part of this item
+     *
+     * @return the rendered body
+     */
     @NotNull
     String genBody();
 
+    /**
+     * Render this item to HTML sources
+     *
+     * @return HTML sources
+     */
     @Override
     default @NotNull String toHTML() {
         return genTitle() + genBody();
