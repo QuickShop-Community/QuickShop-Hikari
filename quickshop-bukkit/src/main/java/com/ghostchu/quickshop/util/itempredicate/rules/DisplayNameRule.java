a5e5ed064e0bda04659cd2ecc9e1e5b7752808f3
Ghost_chu <2908803755@qq.com>
2022-05-17 20:37:12 +0800

    Remove common rules

--- quickshop-bukkit/src/main/java/com/ghostchu/quickshop/util/itempredicate/rules/DisplayNameRule.java
+++ quickshop-bukkit/src/main/java/com/ghostchu/quickshop/util/itempredicate/rules/DisplayNameRule.java
@@ -3,22 +3,24 @@ package com.ghostchu.quickshop.util.itempredicate.rules;
 import com.ghostchu.quickshop.util.itempredicate.method.MatchMethod;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
 @AllArgsConstructor
 @Data
-public class StringRule implements TestRule<String> {
+public class DisplayNameRule implements TestRule {
     @NotNull
     private final MatchMethod method;
     @NotNull
     private final String value;
 
-    public boolean test(@NotNull String tester) {
+    public boolean test(@NotNull ItemStack tester) {
+        String display = tester.getItemMeta().getDisplayName();
         return switch (method) {
-            case EQUALS -> value.equals(tester);
-            case NOT_EQUALS -> !value.equals(tester);
-            case EXCLUDE -> !value.contains(tester);
-            case INCLUDE -> value.contains(tester);
+            case EQUALS -> value.equals(display);
+            case NOT_EQUALS -> !value.equals(display);
+            case EXCLUDE -> !value.contains(display);
+            case INCLUDE -> value.contains(display);
             default -> throw new UnsupportedOperationException("Unsupported match method: " + method);
         };
     }
