cb3db1e0618789f1a69c5351b7166fcc7f793351
Ghost_chu <2908803755@qq.com>
2022-07-04 14:59:06 +0800

    add database related Product and Version and Drivers info to paste.
    It also collect everything it can read from metadata but ONLY user turn on it by add extra startup flags or system properties for debugging different arch or different encoding database issue about escape and keywords and cursor moving support etc.

---
+++ quickshop-bukkit/src/main/java/com/ghostchu/quickshop/util/paste/item/DatabaseInfoItem.java
@@ -0,0 +1,76 @@
+package com.ghostchu.quickshop.util.paste.item;
+
+import com.ghostchu.quickshop.QuickShop;
+import com.ghostchu.quickshop.util.Util;
+import com.ghostchu.quickshop.util.paste.util.HTMLTable;
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.reflect.Method;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+
+public class DatabaseInfoItem implements SubPasteItem {
+    @Override
+    public @NotNull String getTitle() {
+        return "Database";
+    }
+
+    @NotNull
+    private String buildContent() {
+        try (Connection conn = QuickShop.getInstance().getSqlManager().getConnection()) {
+            DatabaseMetaData meta = conn.getMetaData();
+            HTMLTable table = new HTMLTable(2, true);
+            table.insert("Product", meta.getDatabaseProductName());
+            table.insert("Version", meta.getDatabaseProductVersion());
+            table.insert("Driver", meta.getDriverName());
+            table.insert("Driver Version", meta.getDriverVersion());
+            if (Util.parsePackageProperly("generateDatabaseFullReport").asBoolean()) {
+                processFullReportGenerate(meta, table);
+            }
+            return table.render();
+        } catch (SQLException exception) {
+            return "<p>Failed to connect to database or getting metadata.</p>";
+        }
+    }
+
+    private void processFullReportGenerate(@NotNull DatabaseMetaData meta, @NotNull HTMLTable table) {
+        List<Class<?>> allowedClasses = Arrays.asList(
+                String.class,
+                Boolean.class,
+                Long.class,
+                Integer.class,
+                Short.class,
+                Float.class,
+                Double.class,
+                Byte.class,
+                Character.class
+        );
+        for (Method method : meta.getClass().getDeclaredMethods()) {
+            if (method.canAccess(meta)) {
+                if (allowedClasses.contains(method.getReturnType())) {
+                    try {
+                        Object value = method.invoke(meta);
+                        if (value != null) {
+                            table.insert("[verbose] " + method.getName(), value.toString());
+                        } else {
+                            table.insert("[verbose] " + method.getName(), "null");
+                        }
+                    } catch (Exception exception) {
+                        //ignore
+                        table.insert("[verbose] " + method.getName(), exception.getMessage());
+                    }
+                }
+            }
+        }
+
+    }
+
+
+    @Override
+    public @NotNull String genBody() {
+        return buildContent();
+    }
+}
