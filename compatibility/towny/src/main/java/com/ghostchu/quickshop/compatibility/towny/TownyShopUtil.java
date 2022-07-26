cf2183d134de297ad2c63808610fdb277b9c770f
Ghost_chu <2908803755@qq.com>
2022-07-26 20:18:36 +0800

    #372 #374 TownyShopUtil

---
+++ compatibility/towny/src/main/java/com/ghostchu/quickshop/compatibility/towny/TownyShopUtil.java
@@ -0,0 +1,69 @@
+package com.ghostchu.quickshop.compatibility.towny;
+
+import com.ghostchu.quickshop.api.shop.Shop;
+import com.ghostchu.quickshop.util.Util;
+import com.palmergames.bukkit.towny.TownyAPI;
+import com.palmergames.bukkit.towny.object.Nation;
+import com.palmergames.bukkit.towny.object.Town;
+import org.bukkit.configuration.ConfigurationSection;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.UUID;
+
+public class TownyShopUtil {
+    @NotNull
+    public static UUID getShopOriginalOwner(@NotNull Shop shop) {
+        ConfigurationSection section = shop.getExtra(Main.getPlugin(Main.class));
+        if (section.isSet("towny-original-owner")) {
+            return UUID.fromString(section.getString("towny-original-owner", Util.getNilUniqueId().toString()));
+        } else {
+            return shop.getOwner();
+        }
+    }
+
+    public static void setShopOriginalOwner(@NotNull Shop shop, @Nullable UUID owner) {
+        ConfigurationSection section = shop.getExtra(Main.getPlugin(Main.class));
+        if (owner == null) {
+            section.set("towny-original-owner", null);
+        } else {
+            section.set("towny-original-owner", owner.toString());
+        }
+    }
+
+    public static void setShopTown(@NotNull Shop shop, @Nullable Town town) {
+        ConfigurationSection section = shop.getExtra(Main.getPlugin(Main.class));
+        if (town == null) {
+            section.set("towny-town-uuid", null);
+        } else {
+            section.set("towny-town-uuid", town.getUUID());
+        }
+    }
+
+    public static void setShopNation(@NotNull Shop shop, @Nullable Nation nation) {
+        ConfigurationSection section = shop.getExtra(Main.getPlugin(Main.class));
+        if (nation == null) {
+            section.set("towny-nation-uuid", null);
+        } else {
+            section.set("towny-nation-uuid", nation.getUUID());
+        }
+    }
+
+    @Nullable
+    public static Town getShopTown(@NotNull Shop shop) {
+        ConfigurationSection section = shop.getExtra(Main.getPlugin(Main.class));
+        String uuid = section.getString("towny-town-uuid");
+        if (uuid == null)
+            return null;
+        return TownyAPI.getInstance().getTown(uuid);
+    }
+    @Nullable
+    public static Nation getShopNation(@NotNull Shop shop) {
+        ConfigurationSection section = shop.getExtra(Main.getPlugin(Main.class));
+        String uuid = section.getString("towny-nation-uuid");
+        if (uuid == null)
+            return null;
+        return TownyAPI.getInstance().getNation(uuid);
+    }
+}
+
