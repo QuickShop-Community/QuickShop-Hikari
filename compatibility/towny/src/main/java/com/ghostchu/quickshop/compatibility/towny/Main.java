f6120ac3f474a5fc4eb2b16183fff3a3fc25a3c5
Ghost_chu <2908803755@qq.com>
2022-07-28 17:15:52 +0800

    fix workaround won't active in right time

--- compatibility/towny/src/main/java/com/ghostchu/quickshop/compatibility/towny/Main.java
+++ compatibility/towny/src/main/java/com/ghostchu/quickshop/compatibility/towny/Main.java
@@ -51,7 +51,7 @@ public final class Main extends CompatibilityModule implements Listener {
     @NotNull
     public static String processTownyAccount(String accountName) {
         String providerName = QuickShop.getInstance().getEconomy().getProviderName();
-        if (Main.getPlugin(Main.class).getConfig().getBoolean("workaround-for-account-name") && "Essentials".equals(providerName)) {
+        if (Main.getPlugin(Main.class).getConfig().getBoolean("workaround-for-account-name") || "Essentials".equals(providerName)) {
             return EssStringUtil.safeString(accountName);
         }
         return accountName;
