c27781b95a6b82a4cd1171fd1cb7cc72c3a2216d
Ghost_chu <2908803755@qq.com>
2022-08-11 02:07:40 +0800

    tweak the thread pool arguments

--- quickshop-bukkit/src/main/java/com/ghostchu/quickshop/util/QuickExecutor.java
+++ quickshop-bukkit/src/main/java/com/ghostchu/quickshop/util/QuickExecutor.java
@@ -1,26 +1,15 @@
 package com.ghostchu.quickshop.util;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class QuickExecutor {
-    private static final ExecutorService databaseExecutor = Executors.newFixedThreadPool(
-            Math.max(8, Runtime.getRuntime().availableProcessors() * 4), (r) -> {
-                Thread thread = new Thread(r, "QuickShop-Hikari-Database-Pool");
-                thread.setDaemon(true);
-                return thread;
-            });
-    private static final ExecutorService shopSaveExecutor = Executors.newFixedThreadPool(
-            3, (r) -> {
-                Thread thread = new Thread(r, "QuickShop-Hikari-AutoSave-Pool");
-                thread.setDaemon(true);
-                return thread;
-            });
-    private static final ExecutorService commonExecutor = Executors.newFixedThreadPool(Math.max(4, Runtime.getRuntime().availableProcessors() * 2), (r) -> {
-        Thread thread = new Thread(r, "QuickShop-Hikari-Common-Pool");
+    private static final ExecutorService databaseExecutor = new ThreadPoolExecutor(2, 16, 60L, TimeUnit.SECONDS, new SynchronousQueue<>());
+    private static final ExecutorService shopSaveExecutor = Executors.newFixedThreadPool(3, (r) -> {
+        Thread thread = new Thread(r, "QuickShop-Hikari-AutoSave-Pool");
         thread.setDaemon(true);
         return thread;
     });
+    private static final ExecutorService commonExecutor = new ThreadPoolExecutor(0, 16, 60L, TimeUnit.SECONDS, new SynchronousQueue<>());
 
     public static ExecutorService getCommonExecutor() {
         return commonExecutor;
