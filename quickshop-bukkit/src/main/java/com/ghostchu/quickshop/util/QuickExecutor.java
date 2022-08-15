69ee10fd6488d1aaa340060d12cb4689e228141e
Ghost_chu <2908803755@qq.com>
2022-08-15 18:44:55 +0800

    change the QuickExecutor policy

--- quickshop-bukkit/src/main/java/com/ghostchu/quickshop/util/QuickExecutor.java
+++ quickshop-bukkit/src/main/java/com/ghostchu/quickshop/util/QuickExecutor.java
@@ -1,15 +1,14 @@
 package com.ghostchu.quickshop.util;
 
-import java.util.concurrent.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class QuickExecutor {
-    private static final ExecutorService databaseExecutor = new ThreadPoolExecutor(2, 16, 60L, TimeUnit.SECONDS, new SynchronousQueue<>());
-    private static final ExecutorService shopSaveExecutor = Executors.newFixedThreadPool(3, (r) -> {
-        Thread thread = new Thread(r, "QuickShop-Hikari-AutoSave-Pool");
-        thread.setDaemon(true);
-        return thread;
-    });
-    private static final ExecutorService commonExecutor = new ThreadPoolExecutor(1, 16, 60L, TimeUnit.SECONDS, new SynchronousQueue<>());
+    private static final ExecutorService databaseExecutor = new ThreadPoolExecutor(2, 16, 60L, TimeUnit.SECONDS, new LinkedBlockingQueue<>());
+    private static final ExecutorService shopSaveExecutor = new ThreadPoolExecutor(0, 16, 60L, TimeUnit.SECONDS, new LinkedBlockingQueue<>());
+    private static final ExecutorService commonExecutor = new ThreadPoolExecutor(1, 16, 60L, TimeUnit.SECONDS, new LinkedBlockingQueue<>());
 
     public static ExecutorService getCommonExecutor() {
         return commonExecutor;
