b1cf77f4cb1cb5656be6f70bce5c5d273e0ce436
Ghost_chu <2908803755@qq.com>
2022-10-07 20:18:11 +0800

    comment out verbose debug messages

--- quickshop-bukkit/src/main/java/com/ghostchu/quickshop/shop/signhooker/SignHooker.java
+++ quickshop-bukkit/src/main/java/com/ghostchu/quickshop/shop/signhooker/SignHooker.java
@@ -7,16 +7,15 @@ import com.comphenix.protocol.events.ListenerPriority;
 import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketEvent;
 import com.comphenix.protocol.reflect.StructureModifier;
-import com.comphenix.protocol.wrappers.nbt.NbtBase;
 import com.ghostchu.quickshop.QuickShop;
 import com.ghostchu.quickshop.api.GameVersion;
 import com.ghostchu.quickshop.api.shop.Shop;
 import com.ghostchu.quickshop.util.Util;
 import com.ghostchu.quickshop.util.logger.Log;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.block.Sign;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
@@ -33,7 +32,7 @@ public class SignHooker {
 
     private PacketAdapter chunkAdapter;
 
-    private PacketAdapter signAdapter;
+    //private PacketAdapter signAdapter;
 
     public SignHooker(QuickShop plugin) {
         PLUGIN = plugin;
@@ -70,20 +69,19 @@ public class SignHooker {
             }
         };
 
-        signAdapter = new PacketAdapter(PLUGIN, ListenerPriority.HIGH, PacketType.Play.Server.TILE_ENTITY_DATA) {
-            @Override
-            public void onPacketSending(@NotNull PacketEvent event) {
-                NbtBase<?> tField = event.getPacket().getNbtModifier().readSafely(0);
-                // Server send standard sign NBT format for line1, line2, line3, line4 to client.
-                // If we hook that packet and modify component text inside, we can dynamically change the text
-                // that send to client and that will can prevent text blink.
-                // TODO Location deserialize.
-                Log.debug(tField.getName() + " => " + tField.getValue().toString());
-            }
-        };
+//        signAdapter = new PacketAdapter(PLUGIN, ListenerPriority.HIGH, PacketType.Play.Server.TILE_ENTITY_DATA) {
+//            @Override
+//            public void onPacketSending(@NotNull PacketEvent event) {
+//                NbtBase<?> tField = event.getPacket().getNbtModifier().readSafely(0);
+//                // Server send standard sign NBT format for line1, line2, line3, line4 to client.
+//                // If we hook that packet and modify component text inside, we can dynamically change the text
+//                // that send to client and that will can prevent text blink.
+//                // TODO Location deserialize.
+//            }
+//        };
         PROTOCOL_MANAGER.addPacketListener(chunkAdapter);
         Log.debug("SignHooker chunk adapter registered.");
-        PROTOCOL_MANAGER.addPacketListener(signAdapter);
+        //PROTOCOL_MANAGER.addPacketListener(signAdapter);
     }
 
     public void unload(){
@@ -91,7 +89,7 @@ public class SignHooker {
             return;
         if(this.chunkAdapter != null){
             PROTOCOL_MANAGER.removePacketListener(chunkAdapter);
-            PROTOCOL_MANAGER.removePacketListener(signAdapter);
+            //PROTOCOL_MANAGER.removePacketListener(signAdapter);
         }
     }
 
@@ -101,15 +99,17 @@ public class SignHooker {
         if (!Util.isLoaded(location)) return;
         Log.debug("Updating per-player packet sign: Player=" + player.getName() + ", Location=" + location + ", Shop=" + shop.getShopId());
         List<Component> lines = shop.getSignText(PLUGIN.getTextManager().findRelativeLanguages(player));
-        Log.debug(lines.stream().map(component -> GsonComponentSerializer.gson().serialize(component)).toList().toString());
+        //Log.debug(lines.stream().map(component -> GsonComponentSerializer.gson().serialize(component)).toList().toString());
         for (Sign sign : shop.getSigns()) {
-            Log.debug("Target block sign is " + sign.getLocation());
+            //Log.debug("Target block sign is " + sign.getLocation());
             PLUGIN.getPlatform().sendSignTextChange(player, sign, PLUGIN.getConfig().getBoolean("shop.sign-glowing"), lines);
         }
     }
 
     public void updatePerPlayerShopSignBroadcast(Location location, Shop shop) {
-        Collection<Entity> nearbyPlayers = shop.getLocation().getWorld().getNearbyEntities(shop.getLocation(), PLUGIN.getServer().getViewDistance() * 16, shop.getLocation().getWorld().getMaxHeight(), PLUGIN.getServer().getViewDistance() * 16);
+        World world = shop.getLocation().getWorld();
+        if(world == null) return;
+        Collection<Entity> nearbyPlayers = world.getNearbyEntities(shop.getLocation(), PLUGIN.getServer().getViewDistance() * 16, shop.getLocation().getWorld().getMaxHeight(), PLUGIN.getServer().getViewDistance() * 16);
         for (Entity nearbyPlayer : nearbyPlayers) {
             if (nearbyPlayer instanceof Player player) {
                 updatePerPlayerShopSign(player, location, shop);
