7b73ce05c25b84cc7d6e040858b2d6b4bb006363
Ghost_chu <2908803755@qq.com>
2022-10-16 19:56:20 +0800

    More logs in paste

---
+++ quickshop-bukkit/src/main/java/com/ghostchu/quickshop/util/paste/item/CronLogsItem.java
@@ -0,0 +1,47 @@
+package com.ghostchu.quickshop.util.paste.item;
+
+import com.ghostchu.quickshop.QuickShop;
+import com.ghostchu.quickshop.common.util.CommonUtil;
+import com.ghostchu.quickshop.util.MsgUtil;
+import com.ghostchu.quickshop.util.logger.Log;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import org.apache.commons.text.StringEscapeUtils;
+import org.jetbrains.annotations.NotNull;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.StringJoiner;
+
+public class CronLogsItem implements SubPasteItem {
+    private SimpleDateFormat format;
+
+    public CronLogsItem() {
+        String timeFormat = LegacyComponentSerializer.legacySection().serialize(QuickShop.getInstance().text().of("timeunit.std-time-format").forLocale(MsgUtil.getDefaultGameLanguageCode()));
+        try {
+            format = new SimpleDateFormat(timeFormat);
+        } catch (IllegalArgumentException e) {
+            format = new SimpleDateFormat("HH:mm:ss");
+        }
+    }
+
+    @Override
+    public @NotNull String genBody() {
+        return buildContent();
+    }
+
+    @Override
+    public @NotNull String getTitle() {
+        return "Cron History";
+    }
+
+    @NotNull
+    private String buildContent() {
+        StringJoiner builder = new StringJoiner("\n");
+        List<String> debugLogs = Log.fetchLogsExclude(Log.Type.CRON).stream().map(record -> "[" + format.format(record.getTimestamp()) + "] " + record).toList();
+        List<String> tail = CommonUtil.tail(debugLogs, 1000);
+        tail.forEach(builder::add);
+        return "<textarea name=\"debuglogs\" style=\"height: 1000px; width: 100%;\">" +
+                StringEscapeUtils.escapeHtml4(builder.toString()) +
+                "</textarea><br />";
+    }
+}
