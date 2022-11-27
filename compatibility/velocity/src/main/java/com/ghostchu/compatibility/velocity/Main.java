e4cc894b1a1653c6ac484bebe95085138e0187da
Ghost_chu <2908803755@qq.com>
2022-11-27 15:06:20 +0800

    use whitelist mode for signing check

--- compatibility/velocity/src/main/java/com/ghostchu/compatibility/velocity/Main.java
+++ compatibility/velocity/src/main/java/com/ghostchu/compatibility/velocity/Main.java
@@ -19,10 +19,9 @@ import com.velocitypowered.api.proxy.ProxyServer;
 import com.velocitypowered.api.proxy.ServerConnection;
 import com.velocitypowered.api.proxy.messages.MinecraftChannelIdentifier;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
+
+import static com.velocitypowered.api.network.ProtocolVersion.*;
 
 public final class Main {
     private static final MinecraftChannelIdentifier QUICKSHOP_BUNGEE_CHANNEL = MinecraftChannelIdentifier.create("quickshop", "bungee");
@@ -31,6 +30,43 @@ public final class Main {
     private static final String CHAT_COMMAND_REQUEST = "request";
     private static final String CHAT_COMMAND_CANCEL = "cancel";
     private final Set<UUID> pendingForward = Collections.synchronizedSet(new HashSet<>());
+    private static final List<ProtocolVersion> NO_SIGN_VERSIONS = List.of(
+            LEGACY,
+            MINECRAFT_1_7_2,
+            MINECRAFT_1_7_6,
+            MINECRAFT_1_8,
+            MINECRAFT_1_9,
+            MINECRAFT_1_9_1,
+            MINECRAFT_1_9_2,
+            MINECRAFT_1_9_4,
+            MINECRAFT_1_10,
+            MINECRAFT_1_11,
+            MINECRAFT_1_11_1,
+            MINECRAFT_1_12,
+            MINECRAFT_1_12_1,
+            MINECRAFT_1_12_2,
+            MINECRAFT_1_13,
+            MINECRAFT_1_13_1,
+            MINECRAFT_1_13_2,
+            MINECRAFT_1_14,
+            MINECRAFT_1_14_1,
+            MINECRAFT_1_14_2,
+            MINECRAFT_1_14_3,
+            MINECRAFT_1_14_4,
+            MINECRAFT_1_15,
+            MINECRAFT_1_15_1,
+            MINECRAFT_1_15_2,
+            MINECRAFT_1_16,
+            MINECRAFT_1_16_1,
+            MINECRAFT_1_16_2,
+            MINECRAFT_1_16_3,
+            MINECRAFT_1_16_4,
+            MINECRAFT_1_17,
+            MINECRAFT_1_17_1,
+            MINECRAFT_1_18,
+            MINECRAFT_1_18_2,
+            MINECRAFT_1_19
+    );
 
     @Inject
     private ProxyServer proxy;
@@ -87,10 +123,9 @@ public final class Main {
             forwardMessage(player, event.getMessage());
             // Workaround against kicking of players with valid signed key and client 1.19.1 or higher by velocity
             ProtocolVersion protocol = player.getProtocolVersion();
-            if (player.getIdentifiedKey() == null || !protocol.equals(ProtocolVersion.MINECRAFT_1_19_1)) {
+            if (player.getIdentifiedKey() == null || NO_SIGN_VERSIONS.contains(protocol)) {
                 event.setResult(PlayerChatEvent.ChatResult.denied());
             }
-
         }
     }
 
