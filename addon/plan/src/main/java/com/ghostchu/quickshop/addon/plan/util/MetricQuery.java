7c99383fa08b1e4276875fd2238cbedba35c82f0
Ghost_chu <2908803755@qq.com>
2022-12-18 17:06:01 +0800

    clean up

--- addon/plan/src/main/java/com/ghostchu/quickshop/addon/plan/util/MetricQuery.java
+++ addon/plan/src/main/java/com/ghostchu/quickshop/addon/plan/util/MetricQuery.java
@@ -72,7 +72,7 @@ public class MetricQuery {
     // Use LinkedHashMap forced because we need keep the order.
     public @NotNull LinkedHashMap<ShopMetricRecord, DataRecord> mapToDataRecord(@NotNull List<ShopMetricRecord> metricRecords) throws ExecutionException, InterruptedException {
         // map ShopMetricRecord#getShopId to DataRecord with blocking future
-        LinkedHashMap<ShopMetricRecord, DataRecord> dataRecords = new LinkedHashMap();
+        LinkedHashMap<ShopMetricRecord, DataRecord> dataRecords = new LinkedHashMap<>();
         for (ShopMetricRecord metricRecord : metricRecords) {
             long shopId = metricRecord.getShopId();
             Long dataId = databaseHelper.locateShopDataId(shopId).get();
@@ -115,116 +115,5 @@ public class MetricQuery {
             return list;
         }
         return list;
-    }
-
-    //
-//
-//
-//
-//
-//
-//
-//    @NotNull
-//    public List<MetricRecord> queryPlayerPurchase(@NotNull UUID player, long timeStart, long timeEnd, boolean descending, @NotNull ShopOperationEnum... filter) {
-//        try (SQLQuery query = preparePlayerPurchaseQuery(player, timeStart, timeEnd, -1, descending, filter)) {
-//            return wrap(query.getResultSet());
-//        } catch (SQLException e) {
-//            Log.debug("Failed to perform player query on metrics table: " + e.getMessage());
-//            return Collections.emptyList();
-//        }
-//    }
-//
-//    public long queryPlayerPurchaseCount(@NotNull UUID player, long timeStart, long timeEnd, boolean descending, @NotNull ShopOperationEnum... filter) {
-//        try (SQLQuery query = preparePlayerPurchaseQuery(player, timeStart, timeEnd, -1, descending, filter)) {
-//            return JdbcStream.stream(query.getResultSet()).count();
-//        } catch (SQLException e) {
-//            Log.debug("Failed to perform player query on metrics table: " + e.getMessage());
-//            return -1;
-//        }
-//    }
-//
-//    @NotNull
-//    public List<MetricRecord> queryShopTransaction(@NotNull Shop shop, long timeStart, long timeEnd, int limit, boolean descending, @NotNull ShopOperationEnum... filter) {
-//        try (SQLQuery query = prepareShopPurchaseQuery(shop, timeStart, timeEnd, limit, descending, filter)) {
-//            return wrap(query.getResultSet());
-//        } catch (SQLException e) {
-//            Log.debug("Failed to perform shop query on metrics table: " + e.getMessage());
-//            return Collections.emptyList();
-//        }
-//    }
-//
-//    public long queryShopTranslationCount(@NotNull Shop shop, long timeStart, long timeEnd, int limit, boolean descending, @NotNull ShopOperationEnum... filter) {
-//        try (SQLQuery query = prepareShopPurchaseQuery(shop, timeStart, timeEnd, limit, descending, filter)) {
-//            return JdbcStream.stream(query.getResultSet()).count();
-//        } catch (SQLException e) {
-//            Log.debug("Failed to perform player query on metrics table: " + e.getMessage());
-//            return -1;
-//        }
-//    }
-//
-//
-//
-//
-    private String createFilterArgs(@NotNull ShopOperationEnum... filter) {
-        StringJoiner joiner = new StringJoiner(" OR ");
-        for (int i = 0; i < filter.length; i++) {
-            joiner.add("type=?");
-        }
-        return joiner.toString();
-    }
-
-    //
-//    @NotNull
-//    private SQLQuery preparePlayerPurchaseQuery(@NotNull UUID player, long timeStart, long timeEnd, int limit, boolean descending, @NotNull ShopOperationEnum... filter) throws SQLException {
-//        String sql;
-//        if (limit == -1 || limit == Integer.MAX_VALUE) {
-//            sql = "SELECT * FROM " + databaseHelper.getPrefix() + "metrics WHERE player=? AND time>=? AND time<=? AND (" + createFilterArgs(filter) + ") ORDER BY time " + (descending ? "DESC" : "ASC");
-//        } else {
-//            sql = "SELECT * FROM " + databaseHelper.getPrefix() + "metrics WHERE player=? AND time>=? AND time<=? AND (" + createFilterArgs(filter) + ") ORDER BY time " + (descending ? "DESC" : "ASC") + " LIMIT " + limit;
-//        }
-//        List<Object> params = new ArrayList<>();
-//        params.add(player);
-//        params.add(timeStart);
-//        params.add(timeEnd);
-//        params.addAll(Arrays.stream(filter).map(ShopOperationEnum::name).toList());
-//        return databaseHelper.getManager().createQuery().withPreparedSQL(sql).setParams(params).execute();
-//    }
-//
-//    @NotNull
-//    private SQLQuery prepareShopPurchaseQuery(@NotNull Shop shop, long timeStart, long timeEnd, int limit, boolean descending, @NotNull ShopOperationEnum... filter) throws SQLException {
-//        String sql;
-//        if (limit == -1 || limit == Integer.MAX_VALUE) {
-//            sql = "SELECT * FROM " + databaseHelper.getPrefix() + "metrics WHERE x=? AND y=? AND z=? AND world= ? AND time>=? AND time<=? AND (" + createFilterArgs(filter) + ") ORDER BY time " + (descending ? "DESC" : "ASC");
-//        } else {
-//            sql = "SELECT * FROM " + databaseHelper.getPrefix() + "metrics WHERE x=? AND y=? AND z=? AND world= ? AND time>=? AND time<=? AND (" + createFilterArgs(filter) + ") ORDER BY time " + (descending ? "DESC" : "ASC") + " LIMIT " + limit;
-//        }
-//        List<Object> params = new ArrayList<>();
-//        params.add(shop.getLocation().getBlockX());
-//        params.add(shop.getLocation().getBlockY());
-//        params.add(shop.getLocation().getBlockZ());
-//        params.add(shop.getLocation().getWorld().getName());
-//        params.add(timeStart);
-//        params.add(timeEnd);
-//        params.addAll(Arrays.stream(filter).map(ShopOperationEnum::name).toList());
-//        return databaseHelper.getManager().createQuery().withPreparedSQL(sql).setParams(params).execute();
-//    }
-//
-//
-//    @NotNull
-//    private List<MetricRecord> wrap(@NotNull ResultSet set) {
-//        return JdbcStream.stream(set).map(rs -> {
-//            try {
-//                return new SimpleMetricRecord(rs.getLong("time"), rs.getInt("x"), rs.getInt("y"), rs.getInt("z"), rs.getString("world"), ShopOperationEnum.valueOf(rs.getString("type")), rs.getDouble("total"), rs.getDouble("tax"), rs.getInt("amount"), UUID.fromString(rs.getString("player")));
-//            } catch (SQLException e) {
-//                Log.debug("Failed to perform query on metrics table: " + e.getMessage());
-//                return null;
-//            }
-//        }).filter(Objects::nonNull).map(record -> (MetricRecord) record).toList();
-//    }
-    @NotNull
-    private Date daysAgo(int ago) {
-        Calendar cal = Calendar.getInstance();
-        cal.add(Calendar.DAY_OF_MONTH, -ago);
-        return cal.getTime();
     }
 }
