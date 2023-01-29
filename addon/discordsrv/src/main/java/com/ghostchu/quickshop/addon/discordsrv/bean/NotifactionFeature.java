0e78086284ea1cf6bad6957c7ea78246d7f41427
Ghost_chu <ghostchu@111.com>
2023-01-30 02:39:13 +0800

    Translatable feature name

--- addon/discordsrv/src/main/java/com/ghostchu/quickshop/addon/discordsrv/bean/NotifactionFeature.java
+++ addon/discordsrv/src/main/java/com/ghostchu/quickshop/addon/discordsrv/bean/NotifactionFeature.java
@@ -3,22 +3,24 @@ package com.ghostchu.quickshop.addon.discordsrv.bean;
 import org.jetbrains.annotations.NotNull;
 
 public enum NotifactionFeature {
-    USER_SHOP_PERMISSION_CHANGED("notify-shop-permission-changed", "user.shop.permission_changed"),
-    USER_SHOP_PRICE_CHANGED("notify-shop-price-changed", "user.shop.price_changed"),
-    USER_SHOP_TRANSFER("notify-shop-transfer", "user.shop.transfer"),
-    USER_SHOP_OUT_OF_STOCK("notify-shop-out-of-stock", "user.shop.out_of_stock"),
-    USER_SHOP_OUT_OF_SPACE("notify-shop-out-of-space", "user.shop.out_of_space"),
-    USER_SHOP_PURCHASE("notify-shop-purchase", "user.shop.purchase"),
-    MOD_SHOP_PURCHASE("mod-notify-shop-purchase", "mod.shop.purchase"),
-    MOD_SHOP_PRICE_CHANGED("mod-notify-shop-price-changed", "mod.shop.price_changed"),
-    MOD_SHOP_REMOVED("mod-notify-shop-removed", "mod.shop.removed"),
-    MOD_SHOP_TRANSFER("mod-notify-shop-transfer", "mod.shop.trasfer");
+    USER_SHOP_PERMISSION_CHANGED("notify-shop-permission-changed", "user.shop.permission_changed", true),
+    USER_SHOP_PRICE_CHANGED("notify-shop-price-changed", "user.shop.price_changed", true),
+    USER_SHOP_TRANSFER("notify-shop-transfer", "user.shop.transfer", true),
+    USER_SHOP_OUT_OF_STOCK("notify-shop-out-of-stock", "user.shop.out_of_stock", true),
+    USER_SHOP_OUT_OF_SPACE("notify-shop-out-of-space", "user.shop.out_of_space", true),
+    USER_SHOP_PURCHASE("notify-shop-purchase", "user.shop.purchase", true),
+    MOD_SHOP_PURCHASE("mod-notify-shop-purchase", "mod.shop.purchase", false),
+    MOD_SHOP_PRICE_CHANGED("mod-notify-shop-price-changed", "mod.shop.price_changed", false),
+    MOD_SHOP_REMOVED("mod-notify-shop-removed", "mod.shop.removed", false),
+    MOD_SHOP_TRANSFER("mod-notify-shop-transfer", "mod.shop.trasfer", false);
     private final String databaseNode;
     private final String configNode;
+    private final boolean playerToggleable;
 
-    NotifactionFeature(String configNode, String databaseNode) {
+    NotifactionFeature(String configNode, String databaseNode, boolean playerToggleable) {
         this.configNode = configNode;
         this.databaseNode = databaseNode;
+        this.playerToggleable = playerToggleable;
     }
 
     @NotNull
@@ -29,5 +31,9 @@ public enum NotifactionFeature {
     @NotNull
     public String getDatabaseNode() {
         return databaseNode;
+    }
+
+    public boolean isPlayerToggleable() {
+        return playerToggleable;
     }
 }
--- addon/discordsrv/src/main/java/com/ghostchu/quickshop/addon/discordsrv/command/SubCommand_Discord.java
+++ addon/discordsrv/src/main/java/com/ghostchu/quickshop/addon/discordsrv/command/SubCommand_Discord.java
@@ -6,6 +6,7 @@ import com.ghostchu.quickshop.addon.discordsrv.bean.NotifactionFeature;
 import com.ghostchu.quickshop.api.command.CommandHandler;
 import com.ghostchu.quickshop.util.Util;
 import com.ghostchu.quickshop.util.logger.Log;
+import net.kyori.adventure.text.Component;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -41,7 +42,8 @@ public class SubCommand_Discord implements CommandHandler<Player> {
             try {
                 Integer i = plugin.getDatabaseHelper().setNotifactionFeatureEnabled(sender.getUniqueId(), feature, ops);
                 Log.debug("Execute Discord Notifaction with id " + i + " affected");
-                qs.text().of(sender, "addon.discord.feature-status-changed", feature.getConfigNode(), ops).send();
+                Component text = qs.text().of(sender, "addon.discord.message-type-mapping." + feature.getConfigNode(), feature.getConfigNode(), ops).forLocale();
+                qs.text().of(sender, "addon.discord.feature-status-changed", text, ops).send();
             } catch (SQLException e) {
                 qs.text().of(sender, "addon.discord.save-notifaction-exception").send();
                 plugin.getLogger().log(Level.WARNING, "Cannot save the player Discord notification feature status", e);
@@ -52,12 +54,16 @@ public class SubCommand_Discord implements CommandHandler<Player> {
     @Nullable
     private NotifactionFeature getFeatureByName(String name) {
         for (NotifactionFeature value : NotifactionFeature.values()) {
+            NotifactionFeature selected = null;
             if (value.name().equalsIgnoreCase(name))
-                return value;
-            if (value.getConfigNode().equalsIgnoreCase(name))
-                return value;
-            if (value.getDatabaseNode().equalsIgnoreCase(name))
-                return value;
+                selected = value;
+            else if (value.getConfigNode().equalsIgnoreCase(name))
+                selected = value;
+            else if (value.getDatabaseNode().equalsIgnoreCase(name))
+                selected = value;
+            if (selected != null)
+                if (selected.isPlayerToggleable())
+                    return selected;
         }
         return null;
     }
@@ -65,7 +71,10 @@ public class SubCommand_Discord implements CommandHandler<Player> {
     @Override
     public @Nullable List<String> onTabComplete(@NotNull Player sender, @NotNull String commandLabel, @NotNull String[] cmdArg) {
         if (cmdArg.length == 1) {
-            return Arrays.stream(NotifactionFeature.values()).map(NotifactionFeature::getConfigNode).toList();
+            return Arrays.stream(NotifactionFeature.values())
+                    .filter(NotifactionFeature::isPlayerToggleable)
+                    .map(NotifactionFeature::getConfigNode)
+                    .toList();
         }
         if (cmdArg.length == 2) {
             return List.of("enable", "disable");
