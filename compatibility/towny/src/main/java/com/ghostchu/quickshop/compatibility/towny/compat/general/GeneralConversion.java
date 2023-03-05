db82f89b2475b89be167bb5e4ef69a5f02105009
Ghost_chu <ghostchu@111.com>
2023-03-05 21:44:33 +0800

    fix build error

--- compatibility/towny/src/main/java/com/ghostchu/quickshop/compatibility/towny/compat/general/GeneralConversion.java
+++ compatibility/towny/src/main/java/com/ghostchu/quickshop/compatibility/towny/compat/general/GeneralConversion.java
@@ -1,7 +1,6 @@
 package com.ghostchu.quickshop.compatibility.towny.compat.general;
 
 import com.ghostchu.quickshop.QuickShop;
-import com.ghostchu.quickshop.compatibility.towny.Main;
 import com.ghostchu.quickshop.compatibility.towny.compat.UuidConversion;
 import com.palmergames.bukkit.towny.object.Nation;
 import com.palmergames.bukkit.towny.object.Town;
@@ -11,13 +10,11 @@ import java.util.UUID;
 public class GeneralConversion implements UuidConversion {
     @Override
     public UUID convertTownyAccount(Town town) {
-        String vaultAccountName = Main.processTownyAccount(town.getAccount().getName());
-        return QuickShop.getInstance().getPlayerFinder().name2Uuid(vaultAccountName);
+        return QuickShop.getInstance().getPlayerFinder().name2Uuid(town.getAccount().getName());
     }
 
     @Override
     public UUID convertTownyAccount(Nation nation) {
-        String vaultAccountName = Main.processTownyAccount(nation.getAccount().getName());
-        return QuickShop.getInstance().getPlayerFinder().name2Uuid(vaultAccountName);
+        return QuickShop.getInstance().getPlayerFinder().name2Uuid(nation.getAccount().getName());
     }
 }
