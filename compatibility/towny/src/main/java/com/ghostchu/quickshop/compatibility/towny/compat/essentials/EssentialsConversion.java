c45a0aa3b62280c8f323f31151239f87e1897fa6
Ghost_chu <ghostchu@111.com>
2023-03-11 22:25:21 +0800

    remove useless essentials checks

--- compatibility/towny/src/main/java/com/ghostchu/quickshop/compatibility/towny/compat/essentials/EssentialsConversion.java
+++ compatibility/towny/src/main/java/com/ghostchu/quickshop/compatibility/towny/compat/essentials/EssentialsConversion.java
@@ -1,12 +1,9 @@
 package com.ghostchu.quickshop.compatibility.towny.compat.essentials;
 
-import com.ghostchu.quickshop.QuickShop;
 import com.ghostchu.quickshop.compatibility.towny.EssStringUtil;
-import com.ghostchu.quickshop.compatibility.towny.Main;
 import com.ghostchu.quickshop.compatibility.towny.compat.UuidConversion;
 import com.palmergames.bukkit.towny.object.Nation;
 import com.palmergames.bukkit.towny.object.Town;
-import org.bukkit.plugin.java.JavaPlugin;
 
 import java.nio.charset.StandardCharsets;
 import java.util.UUID;
@@ -25,10 +22,6 @@ public class EssentialsConversion implements UuidConversion {
     }
 
     private String processAccount(String accountName) {
-        String providerName = QuickShop.getInstance().getEconomy().getProviderName();
-        if (JavaPlugin.getPlugin(Main.class).getConfig().getBoolean("workaround-for-account-name") || "Essentials".equals(providerName)) {
-            return EssStringUtil.safeString(accountName);
-        }
-        return accountName;
+        return EssStringUtil.safeString(accountName);
     }
 }
