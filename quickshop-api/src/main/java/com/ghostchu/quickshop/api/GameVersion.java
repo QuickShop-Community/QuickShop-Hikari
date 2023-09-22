5fd494cc45e836e0a128852b81243b926137412a
Ghost_chu <ghostchu@111.com>
2023-09-22 14:45:34 +0800

    turn display in game-version back true

--- quickshop-api/src/main/java/com/ghostchu/quickshop/api/GameVersion.java
+++ quickshop-api/src/main/java/com/ghostchu/quickshop/api/GameVersion.java
@@ -46,7 +46,7 @@ public enum GameVersion {
     v1_19_R2(true, false, true, true, true, true),
     v1_19_R3(true, false, true, true, true, true),
     v1_20_R1(true, false, true, true, true, true),
-    v1_20_R2(true, false, false, true, true, true),
+    v1_20_R2(true, false, true, true, true, true),
     UNKNOWN(true, false, false, true, true, true);
     /**
      * CoreSupports - Check does QuickShop most features supports this server version
