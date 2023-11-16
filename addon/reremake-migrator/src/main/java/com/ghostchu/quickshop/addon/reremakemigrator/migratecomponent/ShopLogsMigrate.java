1279945f7479aa695cbc2e8a80bddb2795ede474
Ghost_chu <ghostchu@111.com>
2023-11-17 01:08:31 +0800

    Delete temp file after migrate completed

--- addon/reremake-migrator/src/main/java/com/ghostchu/quickshop/addon/reremakemigrator/migratecomponent/ShopLogsMigrate.java
+++ addon/reremake-migrator/src/main/java/com/ghostchu/quickshop/addon/reremakemigrator/migratecomponent/ShopLogsMigrate.java
@@ -49,17 +49,22 @@ public class ShopLogsMigrate extends AbstractMigrateComponent {
         text("modules.shop-logs.start-migrate").send();
         File logsFile = new File(getReremake().getDataFolder(), "appended-qs.log");
         File filteredFile = new File(getReremake().getDataFolder(), "filtered-qs.log");
+        File formattedFile = new File(getReremake().getDataFolder(), "formatted-qs.log");
         try (PrintWriter printWriter = new PrintWriter(logsFile, StandardCharsets.UTF_8)) {
             logsFile.createNewFile();
             filteredFile.createNewFile();
             appendFiles(getReremake().getDataFolder(), printWriter);
             printWriter.flush();
             migrateAppendedLogs(logsFile, filteredFile);
-            readAndFormatEntire(filteredFile);
-            importToDatabase(new File(getReremake().getDataFolder(), "formatted-qs.log"));
+            readAndFormatEntire(filteredFile, formattedFile);
+            importToDatabase(formattedFile);
         } catch (Exception ex) {
             getHikari().logger().warn("Failed to migrate logs", ex);
             return true;
+        } finally {
+            logsFile.delete();
+            filteredFile.delete();
+            formattedFile.delete();
         }
         return true;
     }
@@ -158,9 +163,8 @@ public class ShopLogsMigrate extends AbstractMigrateComponent {
         getHikari().getDatabaseHelper().insertMetricRecord(shopMetricRecord);
     }
 
-    private void readAndFormatEntire(File filteredFile) throws IOException {
+    private void readAndFormatEntire(File filteredFile, File formattedFile) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new FileReader(filteredFile));
-        File formattedFile = new File(getReremake().getDataFolder(), "formatted-qs.log");
         formattedFile.createNewFile();
         PrintWriter writer = new PrintWriter(formattedFile, StandardCharsets.UTF_8);
         StringBuilder buffer = new StringBuilder();
