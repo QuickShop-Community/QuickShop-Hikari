31a3da6a6d6f6256101e4e6f90865a18dc0834b1
Ghost_chu <ghostchu@111.com>
2024-01-29 21:48:20 +0800

    Added ItemAdder compatibility module

---
+++ compatibility/itemadder/src/main/java/com/ghostchu/quickshop/compatibility/itemadder/Main.java
@@ -0,0 +1,36 @@
+package com.ghostchu.quickshop.compatibility.slimefun;
+
+import com.ghostchu.quickshop.api.registry.builtin.itemexpression.ItemExpressionHandler;
+import com.ghostchu.quickshop.compatibility.CompatibilityModule;
+import dev.lone.itemsadder.api.CustomStack;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+
+public final class Main extends CompatibilityModule implements ItemExpressionHandler {
+
+    @Override
+    public void init() {
+        // There no init stuffs need to do
+    }
+
+
+    @Override
+    public @NotNull Plugin getPlugin() {
+        return this;
+    }
+
+    @Override
+    public char getPrefix() {
+        return '➕';
+    }
+
+    @Override
+    public boolean match(ItemStack stack, String expression) {
+        CustomStack customStack = CustomStack.byItemStack(stack);
+        if(customStack == null){
+            return false;
+        }
+        return expression.equals(customStack.getId());
+    }
+}
