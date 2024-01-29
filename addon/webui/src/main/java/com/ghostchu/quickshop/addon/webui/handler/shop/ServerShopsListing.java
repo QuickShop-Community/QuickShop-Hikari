fa69c8f093dfeac61588b8b4f7f42bfc668003e6
Ghost_chu <ghostchu@111.com>
2024-01-30 01:40:50 +0800

    Initial webui framework

---
+++ addon/webui/src/main/java/com/ghostchu/quickshop/addon/webui/handler/shop/ServerShopsListing.java
@@ -0,0 +1,9 @@
+package com.ghostchu.quickshop.addon.webui.handler.shop;
+
+import com.ghostchu.quickshop.addon.webui.handler.GeneralHandler;
+import io.vertx.ext.web.RoutingContext;
+
+public class ServerShopsListing implements GeneralHandler {
+    public ServerShopsListing(RoutingContext ctx) {
+    }
+}
