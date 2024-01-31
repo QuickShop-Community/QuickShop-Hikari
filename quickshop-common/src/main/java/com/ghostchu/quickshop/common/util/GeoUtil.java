6ae17a5c37a9cd25685c6b495cb9850f9647b3cd
Ghost_chu <ghostchu@111.com>
2024-02-01 00:00:35 +0800

    Improve dependencies resolving

--- quickshop-common/src/main/java/com/ghostchu/quickshop/common/util/GeoUtil.java
+++ quickshop-common/src/main/java/com/ghostchu/quickshop/common/util/GeoUtil.java
@@ -1,5 +1,8 @@
 package com.ghostchu.quickshop.common.util;
 
+import com.ghostchu.quickshop.common.util.mirror.MavenCentralMirror;
+import org.jetbrains.annotations.NotNull;
+
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
@@ -9,7 +12,14 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.logging.Logger;
 
 public class GeoUtil {
     private static volatile Boolean inChinaRegion = null;
@@ -27,12 +37,53 @@ public class GeoUtil {
         });
     }
 
-    public static boolean preferChinaServers(){
-        CompletableFuture<Integer> cfTest = connectTest("cloudflare.com", 443, 5*1000);
-        CompletableFuture<Integer> weiboTest = connectTest("weibo.com", 443, 5*1000);
-        int cf = cfTest.join();
-        int weibo = weiboTest.join();
-        return weibo < cf;
+    private static long sendGetTest(String urlStr) {
+        try {
+            HttpClient client = HttpClient.newBuilder()
+                    .connectTimeout(Duration.of(5, ChronoUnit.SECONDS))
+                    .followRedirects(HttpClient.Redirect.ALWAYS)
+                    .build();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI(urlStr))
+                    .timeout(Duration.of(5, ChronoUnit.SECONDS))
+                    .GET()
+                    .build();
+            long time = System.currentTimeMillis();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode() != 200) {
+                return Long.MAX_VALUE;
+            }
+            return System.currentTimeMillis() - time;
+        } catch (Throwable e) {
+            return Long.MAX_VALUE;
+        }
+    }
+
+    @NotNull
+    public static List<MavenCentralMirror> determineBestMirrorServer(Logger logger) {
+        List<CompletableFuture<Void>> testEntry = new ArrayList<>();
+        Map<MavenCentralMirror, Long> mirrorPingMap = new ConcurrentSkipListMap<>();
+        for (MavenCentralMirror value : MavenCentralMirror.values()) {
+            testEntry.add(CompletableFuture.supplyAsync(() -> {
+                mirrorPingMap.put(value, sendGetTest(value.getTestUrl()));
+                return null;
+            }));
+        }
+        testEntry.forEach(CompletableFuture::join);
+        List<Map.Entry<MavenCentralMirror, Long>> list = new ArrayList<>(mirrorPingMap.entrySet());
+        list.sort(Map.Entry.comparingByValue());
+        logger.info("Maven repository mirror test result:");
+        list.forEach(e -> {
+            String cost = "DNF";
+            if (e.getValue() != Long.MAX_VALUE) {
+                cost = e.getValue() + "ms";
+            }
+            logger.info("[" + e.getKey().getRegion() + "] " + e.getKey().name() + ": " + cost);
+        });
+        if (list.isEmpty()) {
+            return Collections.emptyList();
+        }
+        return list.stream().filter(e->e.getValue()!=Long.MAX_VALUE).limit(3).map(Map.Entry::getKey).toList();
     }
 
     public static boolean inChinaRegion() {
