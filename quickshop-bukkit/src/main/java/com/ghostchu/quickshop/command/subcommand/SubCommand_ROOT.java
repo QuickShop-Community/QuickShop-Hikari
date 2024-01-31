f3302469beb5c7cdc167988e0086a5a5a5be0848
Ghost_chu <ghostchu@111.com>
2024-01-31 20:26:54 +0800

    Fix the tab-complete doesn't for normal players if they don't have benefit permission

--- quickshop-bukkit/src/main/java/com/ghostchu/quickshop/command/subcommand/SubCommand_ROOT.java
+++ quickshop-bukkit/src/main/java/com/ghostchu/quickshop/command/subcommand/SubCommand_ROOT.java
@@ -4,13 +4,13 @@ import com.ghostchu.quickshop.QuickShop;
 import com.ghostchu.quickshop.api.command.CommandContainer;
 import com.ghostchu.quickshop.api.command.CommandHandler;
 import com.ghostchu.quickshop.api.command.CommandParser;
+import com.ghostchu.quickshop.common.util.CommonUtil;
 import com.ghostchu.quickshop.util.Util;
 import com.ghostchu.quickshop.util.logger.Log;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class SubCommand_ROOT implements CommandHandler<CommandSender> {
@@ -30,6 +30,7 @@ public class SubCommand_ROOT implements CommandHandler<CommandSender> {
     @Override
     public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull String commandLabel, @NotNull CommandParser parser) {
         final List<String> candidate = new ArrayList<>();
+        container:
         for (CommandContainer container : plugin.getCommandManager().getRegisteredCommands()) {
             if (!parser.getArgs().isEmpty() && !container.getPrefix().startsWith(parser.getArgs().get(0))) {
                 continue;
@@ -39,9 +40,7 @@ public class SubCommand_ROOT implements CommandHandler<CommandSender> {
 
             if (requirePermissions != null) {
                 for (String requirePermission : requirePermissions) {
-                    if (requirePermission != null
-                            && !requirePermission.isEmpty()
-                            && !plugin.perm().hasPermission(sender, requirePermission)) {
+                    if (requirePermission != null && !plugin.perm().hasPermission(sender, requirePermission)) {
                         if (Util.isDevMode()) {
                             Log.debug(
                                     "Player "
@@ -51,8 +50,29 @@ public class SubCommand_ROOT implements CommandHandler<CommandSender> {
                                             + ", but doesn't have the permission: "
                                             + requirePermission);
                         }
-                        return Collections.emptyList();
+                        continue container;
                     }
+                }
+            }
+
+            final List<String> selectivePermissions = container.getSelectivePermissions();
+            boolean anyHit = false;
+            if(selectivePermissions != null){
+                for (String selectivePermission : selectivePermissions) {
+                    if(selectivePermission != null && plugin.perm().hasPermission(sender, selectivePermission)){
+                        anyHit = true;
+                        break;
+                    }
+                }
+                if(!anyHit){
+                    Log.debug(
+                            "Player "
+                                    + sender.getName()
+                                    + " is trying to tab-complete the command: "
+                                    + commandLabel
+                                    + ", but doesn't have any permission in the list: "
+                                    + CommonUtil.list2String(selectivePermissions));
+                    continue;
                 }
             }
 
