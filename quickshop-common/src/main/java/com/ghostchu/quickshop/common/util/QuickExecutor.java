ad2bfa893ba6aea77db25a75559dca978362a0f2
Ghost_chu <ghostchu@111.com>
2024-02-06 02:55:32 +0800

    Use SHOP_HISTORY_QUERY_EXECUTOR

--- quickshop-common/src/main/java/com/ghostchu/quickshop/common/util/QuickExecutor.java
+++ quickshop-common/src/main/java/com/ghostchu/quickshop/common/util/QuickExecutor.java
@@ -3,10 +3,9 @@ package com.ghostchu.quickshop.common.util;
 import java.util.concurrent.*;
 
 public class QuickExecutor {
-
     private static final ExecutorService DATABASE_EXECUTOR = new ThreadPoolExecutor(2, 16, 60L, TimeUnit.SECONDS, new LinkedBlockingQueue<>());
-
-    private static final ExecutorService SHOP_SAVE_EXECUTOR = new ThreadPoolExecutor(0, 16, 60L, TimeUnit.SECONDS, new LinkedBlockingQueue<>());
+    private static final ExecutorService SHOP_HISTORY_QUERY_EXECUTOR = new ThreadPoolExecutor(0, 2, 60L, TimeUnit.SECONDS, new LinkedBlockingQueue<>());
+    private static final ExecutorService SHOP_SAVE_EXECUTOR = new ThreadPoolExecutor(0, 2, 60L, TimeUnit.SECONDS, new LinkedBlockingQueue<>());
     private static final ExecutorService COMMON_EXECUTOR = Executors.newCachedThreadPool();
     private static final BlockingQueue<Runnable> PRIMARY_PROFILE_IO_QUEUE = new LinkedBlockingDeque<>();
     private static ExecutorService PRIMARY_PROFILE_IO_EXECUTOR = new ThreadPoolExecutor(2, 32, 60L, TimeUnit.SECONDS, PRIMARY_PROFILE_IO_QUEUE);
@@ -51,5 +50,9 @@ public class QuickExecutor {
 
     public static void setSecondaryProfileIoExecutor(ExecutorService secondaryProfileIoExecutor) {
         SECONDARY_PROFILE_IO_EXECUTOR = secondaryProfileIoExecutor;
+    }
+
+    public static ExecutorService getShopHistoryQueryExecutor() {
+        return SHOP_HISTORY_QUERY_EXECUTOR;
     }
 }
