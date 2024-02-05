6f1736b3c7c23ce767ba004eca17e3812614c72f
Ghost_chu <ghostchu@111.com>
2024-02-06 03:04:45 +0800

    keep at least one executor both for database & history query

--- quickshop-common/src/main/java/com/ghostchu/quickshop/common/util/QuickExecutor.java
+++ quickshop-common/src/main/java/com/ghostchu/quickshop/common/util/QuickExecutor.java
@@ -4,8 +4,8 @@ import java.util.concurrent.*;
 
 public class QuickExecutor {
     private static final ExecutorService DATABASE_EXECUTOR = new ThreadPoolExecutor(2, 16, 60L, TimeUnit.SECONDS, new LinkedBlockingQueue<>());
-    private static final ExecutorService SHOP_HISTORY_QUERY_EXECUTOR = new ThreadPoolExecutor(0, 2, 60L, TimeUnit.SECONDS, new LinkedBlockingQueue<>());
-    private static final ExecutorService SHOP_SAVE_EXECUTOR = new ThreadPoolExecutor(0, 2, 60L, TimeUnit.SECONDS, new LinkedBlockingQueue<>());
+    private static final ExecutorService SHOP_HISTORY_QUERY_EXECUTOR = new ThreadPoolExecutor(1, 2, 60L, TimeUnit.SECONDS, new LinkedBlockingQueue<>());
+    private static final ExecutorService SHOP_SAVE_EXECUTOR = new ThreadPoolExecutor(1, 2, 60L, TimeUnit.SECONDS, new LinkedBlockingQueue<>());
     private static final ExecutorService COMMON_EXECUTOR = Executors.newCachedThreadPool();
     private static final BlockingQueue<Runnable> PRIMARY_PROFILE_IO_QUEUE = new LinkedBlockingDeque<>();
     private static ExecutorService PRIMARY_PROFILE_IO_EXECUTOR = new ThreadPoolExecutor(2, 32, 60L, TimeUnit.SECONDS, PRIMARY_PROFILE_IO_QUEUE);
