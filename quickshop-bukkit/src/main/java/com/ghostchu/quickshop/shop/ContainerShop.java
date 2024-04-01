e8f7d5700b3caf8e0675f8f6677dd4abec2b831c
Ghost_chu <ghostchu@111.com>
2024-04-01 22:38:58 +0800

    Avoid creating a YamlConfiguration when it is not necessary to save memory

--- quickshop-bukkit/src/main/java/com/ghostchu/quickshop/shop/ContainerShop.java
+++ quickshop-bukkit/src/main/java/com/ghostchu/quickshop/shop/ContainerShop.java
@@ -66,13 +66,15 @@ public class ContainerShop implements Shop, Reloadable {
     private static final String LEGACY_SHOP_SIGN_RECOGNIZE_PATTERN = "§d§o ";
     @NotNull
     private final Location location;
-    private final YamlConfiguration extra;
     @EqualsAndHashCode.Exclude
     private final QuickShop plugin;
     @EqualsAndHashCode.Exclude
     private final UUID runtimeRandomUniqueId = UUID.randomUUID();
     @NotNull
     private final Map<UUID, String> playerGroup;
+    @EqualsAndHashCode.Exclude
+    private final boolean isDeleted = false;
+    private YamlConfiguration extra;
     private long shopId;
     private QUser owner;
     private double price;
@@ -87,8 +89,6 @@ public class ContainerShop implements Shop, Reloadable {
     private AbstractDisplayItem displayItem;
     @EqualsAndHashCode.Exclude
     private volatile boolean isLoaded = false;
-    @EqualsAndHashCode.Exclude
-    private final boolean isDeleted = false;
     @EqualsAndHashCode.Exclude
     private volatile boolean createBackup = false;
     @EqualsAndHashCode.Exclude
@@ -165,7 +165,7 @@ public class ContainerShop implements Shop, Reloadable {
             @NotNull QUser owner,
             boolean unlimited,
             @NotNull ShopType type,
-            @NotNull YamlConfiguration extra,
+            @Nullable YamlConfiguration extra,
             @Nullable String currency,
             boolean disableDisplay,
             @Nullable QUser taxAccount,
@@ -415,6 +415,9 @@ public class ContainerShop implements Shop, Reloadable {
      */
     @Override
     public @NotNull ConfigurationSection getExtra(@NotNull Plugin plugin) {
+        if (this.extra == null) {
+            this.extra = new YamlConfiguration();
+        }
         ConfigurationSection section = extra.getConfigurationSection(plugin.getName());
         if (section == null) {
             section = extra.createSection(plugin.getName());
@@ -1246,7 +1249,7 @@ public class ContainerShop implements Shop, Reloadable {
 
     @Override
     public @NotNull String saveExtraToYaml() {
-        return extra.saveToString();
+        return extra == null ? "" : extra.saveToString();
     }
 
     @Override
@@ -1334,7 +1337,27 @@ public class ContainerShop implements Shop, Reloadable {
      */
     @Override
     public void setExtra(@NotNull Plugin plugin, @NotNull ConfigurationSection data) {
+        if (this.extra == null) {
+            this.extra = new YamlConfiguration();
+        }
         extra.set(plugin.getName(), data);
+        // compress extra to null if possible
+        boolean anyValid = false;
+        for (String key : extra.getKeys(false)) {
+            if (!extra.isConfigurationSection(key)) {
+                anyValid = true;
+                break;
+            }
+            ConfigurationSection section = extra.getConfigurationSection(key);
+            if (section == null) continue;
+            if (!section.getKeys(false).isEmpty()) {
+                anyValid = true;
+                break;
+            }
+        }
+        if (!anyValid) {
+            this.extra = null;
+        }
         setDirty();
     }
 
