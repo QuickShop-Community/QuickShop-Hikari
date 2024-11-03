990e4f23b5b81fe6b5f6379a7b52bc178fdc208c
Philip Hell <blvckbytes@gmail.com>
2024-11-03 18:35:48 +0100

    EnvironmentChecker#virtualDisplayWorkingTest no longer fails if dispays are either disabled, or not of type virtual (#1807)
    * EnvironmentChecker#virtualDisplayWorkingTest no longer fails if displays are either disabled, or not of type virtual
    
    * improved pass-messages of virtualDisplaySupportTest and virtualDisplayWorkingTest in EnvironmentChecker

--- quickshop-bukkit/src/main/java/com/ghostchu/quickshop/util/envcheck/EnvironmentChecker.java
+++ quickshop-bukkit/src/main/java/com/ghostchu/quickshop/util/envcheck/EnvironmentChecker.java
@@ -357,10 +357,10 @@ public final class EnvironmentChecker {
   public ResultContainer virtualDisplaySupportTest() {
 
     if(!plugin.isDisplayEnabled()) {
-      return new ResultContainer(CheckResult.PASSED, "The display are disabled.");
+      return new ResultContainer(CheckResult.PASSED, "The setting shop.display-items is disabled.");
     }
     if(AbstractDisplayItem.getNowUsing() != DisplayType.VIRTUALITEM) {
-      return new ResultContainer(CheckResult.PASSED, "The display type is not virtual item.");
+      return new ResultContainer(CheckResult.PASSED, "The setting shop.display-type is not virtual item.");
     }
     if(!plugin.getGameVersion().isVirtualDisplaySupports()) {
       AbstractDisplayItem.setVirtualDisplayDoesntWork(true);
@@ -377,6 +377,12 @@ public final class EnvironmentChecker {
   @EnvCheckEntry(name = "Virtual DisplayItem Support Test", priority = 7, stage = EnvCheckEntry.Stage.AFTER_ON_ENABLE)
   public ResultContainer virtualDisplayWorkingTest() {
 
+    if(!plugin.isDisplayEnabled()) {
+      return new ResultContainer(CheckResult.PASSED, "The setting shop.display-items is disabled.");
+    }
+    if(AbstractDisplayItem.getNowUsing() != DisplayType.VIRTUALITEM) {
+      return new ResultContainer(CheckResult.PASSED, "The setting shop.display-type is not virtual item.");
+    }
     if(plugin.getVirtualDisplayItemManager() == null) {
       AbstractDisplayItem.setVirtualDisplayDoesntWork(true);
       return new ResultContainer(CheckResult.WARNING, "VirtualDisplayItemManager is null, this shouldn't happen, contact with QuickShop-Hikari developer.");
