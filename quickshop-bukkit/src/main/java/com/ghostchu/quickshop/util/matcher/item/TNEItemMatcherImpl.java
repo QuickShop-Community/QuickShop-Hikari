aad954480de76e5dd561efd5a156e15efacb6358
Daniel V. <daniel.viddy@gmail.com>
2025-01-23 14:14:59 -0500

    Update versions of compat/addon/adventure dependencies. Move versions to main pom for easier modification.

--- quickshop-bukkit/src/main/java/com/ghostchu/quickshop/util/matcher/item/TNEItemMatcherImpl.java
+++ quickshop-bukkit/src/main/java/com/ghostchu/quickshop/util/matcher/item/TNEItemMatcherImpl.java
@@ -33,7 +33,7 @@ import org.jetbrains.annotations.Nullable;
  * TNEItemMatcherImpl
  *
  * @author creatorfromhell
- * @since 6.2.0.8-SNAPSHOT-1
+ * @since 6.2.0.8
  */
 public class TNEItemMatcherImpl implements ItemMatcher {
 
