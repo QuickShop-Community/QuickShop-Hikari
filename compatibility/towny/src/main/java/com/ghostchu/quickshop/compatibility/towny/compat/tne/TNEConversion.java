3fc88e387deb5e45de90dff3eeb915209ce5c879
Daniel V. <daniel.viddy@gmail.com>
2025-01-26 22:32:08 -0500

    Update TNEConversion

--- compatibility/towny/src/main/java/com/ghostchu/quickshop/compatibility/towny/compat/tne/TNEConversion.java
+++ compatibility/towny/src/main/java/com/ghostchu/quickshop/compatibility/towny/compat/tne/TNEConversion.java
@@ -40,13 +40,13 @@ public class TNEConversion implements UuidConversion {
   public UUID convertTownyAccount(final Town town) {
 
     final Optional<Account> account = TNECore.api().getAccount(town.getAccount().getName());
-    return account.map(value->UUID.fromString(value.getIdentifier())).orElseGet(()->QuickShop.getInstance().getPlayerFinder().name2Uuid(town.getAccount().getName()));
+    return account.map(Account::getIdentifier).orElseGet(()->QuickShop.getInstance().getPlayerFinder().name2Uuid(town.getAccount().getName()));
   }
 
   @Override
   public UUID convertTownyAccount(final Nation nation) {
 
     final Optional<Account> account = TNECore.api().getAccount(nation.getAccount().getName());
-    return account.map(value->UUID.fromString(value.getIdentifier())).orElseGet(()->QuickShop.getInstance().getPlayerFinder().name2Uuid(nation.getAccount().getName()));
+    return account.map(Account::getIdentifier).orElseGet(()->QuickShop.getInstance().getPlayerFinder().name2Uuid(nation.getAccount().getName()));
   }
 }
