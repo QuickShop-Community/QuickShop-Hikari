9159bb2e8f5f7c1245853c032c3de3f54cfefc99
YuanYuanOwO <yuanyuanowo@gmail.com>
2025-03-12 22:13:16 +0800

    fix

--- compatibility/slimefun/src/main/java/com/ghostchu/quickshop/compatibility/slimefun/Main.java
+++ compatibility/slimefun/src/main/java/com/ghostchu/quickshop/compatibility/slimefun/Main.java
@@ -60,11 +60,27 @@ public final class Main extends CompatibilityModule implements SlimefunAddon, It
 
   @EventHandler
   public void onExplosionPickaxeEvent(final ExplosiveToolBreakBlocksEvent event) {
+    // get the primary block that player tries to break
+    Block primaryBlock = event.getPrimaryBlock();
+    Shop primaryShop = QuickShop.getInstance().getShopManager().getShop(primaryBlock.getLocation());
+
+    // check if it's not null
+    if (primaryShop != null) {
+      // cancel the explosion
+      event.setCancelled(true);
+      return;
+    }
+
+    // get all additional block such as it's radius.
     for (Block block : event.getAdditionalBlocks()) {
+      // get variable for shop
       Shop shop = QuickShop.getInstance().getShopManager().getShop(block.getLocation());
 
+      // check if it's not null
       if (shop != null) {
+        // cancel the explosion on that block
         event.setCancelled(true);
+        return;
       }
     }
   }
