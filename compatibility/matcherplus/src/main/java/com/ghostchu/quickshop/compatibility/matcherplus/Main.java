4ccb1d3e451c33af8e0ed04bec6ae3e02af66b64
Daniel V. <daniel.viddy@gmail.com>
2025-04-25 00:52:08 -0400

    Create MatcherPlus Compatibility module. Transfer over pyrofishingpro namespace.

---
+++ compatibility/matcherplus/src/main/java/com/ghostchu/quickshop/compatibility/matcherplus/Main.java
@@ -0,0 +1,46 @@
+package com.ghostchu.quickshop.compatibility.matcherplus;
+
+import com.ghostchu.quickshop.api.event.general.ShopItemMatchEvent;
+import com.ghostchu.quickshop.compatibility.CompatibilityModule;
+import org.bukkit.NamespacedKey;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.persistence.PersistentDataType;
+
+public final class Main extends CompatibilityModule implements Listener {
+
+
+  @Override
+  public void init() {
+  }
+
+  @EventHandler
+  public void onMatch(final ShopItemMatchEvent event) {
+
+    if(event.original() == null) {
+      return;
+    }
+
+    if(event.comparison() == null) {
+      return;
+    }
+
+    final int originalFish = fishData(event.original());
+    final int testerFish = fishData(event.comparison());
+    if(originalFish > -1 || testerFish > -1) {
+
+      event.matches(originalFish == testerFish);
+      return;
+    }
+
+  }
+
+  public Integer fishData(final ItemStack stack) {
+
+    if(stack.getItemMeta() != null) {
+      return stack.getItemMeta().getPersistentDataContainer().getOrDefault(new NamespacedKey("pyrofishingpro", "fishnumber"), PersistentDataType.INTEGER, -1);
+    }
+    return -1;
+  }
+}
