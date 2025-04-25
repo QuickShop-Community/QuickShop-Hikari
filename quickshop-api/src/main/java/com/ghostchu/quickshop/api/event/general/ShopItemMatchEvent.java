ae26de79f3c7fcd92e4303fdd4995a28aa50bb43
Daniel V. <daniel.viddy@gmail.com>
2025-04-25 00:42:56 -0400

    Fix license header on ShopItemMatchEvent

--- quickshop-api/src/main/java/com/ghostchu/quickshop/api/event/general/ShopItemMatchEvent.java
+++ quickshop-api/src/main/java/com/ghostchu/quickshop/api/event/general/ShopItemMatchEvent.java
@@ -1,6 +1,6 @@
 package com.ghostchu.quickshop.api.event.general;
 /*
- * The New Economy
+ * QuickShop-Hikari
  * Copyright (C) 2025 Daniel "creatorfromhell" Vidmar
  *
  * This program is free software: you can redistribute it and/or modify
@@ -25,7 +25,7 @@ import org.jetbrains.annotations.Nullable;
  * ShopItemMatchEvent
  *
  * @author creatorfromhell
- * @since 1.0.0.0
+ * @since 6.2.0.10
  */
 public class ShopItemMatchEvent extends AbstractQSEvent {
 
