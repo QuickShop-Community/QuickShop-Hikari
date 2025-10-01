1fae12e70a936305f71cb65f29daf16e1d77e7a2
Daniel V. <daniel.viddy@gmail.com>
2025-09-30 20:54:42 -0400

    Update to 1.21.9

--- quickshop-bukkit/src/main/java/com/ghostchu/quickshop/shop/display/virtual/packet/PacketEventsHandler.java
+++ quickshop-bukkit/src/main/java/com/ghostchu/quickshop/shop/display/virtual/packet/PacketEventsHandler.java
@@ -108,6 +108,7 @@ public class PacketEventsHandler implements PacketHandler<PacketEventsAPI<?>> {
     factories.put("1.21.6", oneTwentyOne);
     factories.put("1.21.7", oneTwentyOne);
     factories.put("1.21.8", oneTwentyOne);
+    factories.put("1.21.9", oneTwentyOne);
   }
 
   @Override
