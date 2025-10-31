1c47c645a9fba1f8da1b6525693cc054a7cd3132
MrlingXD <90316914+wling-art@users.noreply.github.com>
2025-10-31 09:21:30 +0800

    fix: PacketEvents ClassNotFoundException with ProtocolLib (#2157)
    * fix: PacketEvents ClassNotFoundException with ProtocolLib
    
    * refactor: replace reflection with polymorphism for enchantment filtering
    
    * style: apply code style guidelines

--- quickshop-bukkit/src/main/java/com/ghostchu/quickshop/shop/display/virtual/VirtualDisplayItem.java
+++ quickshop-bukkit/src/main/java/com/ghostchu/quickshop/shop/display/virtual/VirtualDisplayItem.java
@@ -32,7 +32,6 @@ import com.ghostchu.quickshop.shop.display.AbstractDisplayItem;
 import com.ghostchu.quickshop.util.Util;
 import com.ghostchu.quickshop.util.logger.Log;
 import com.ghostchu.simplereloadlib.Reloadable;
-import com.github.retrooper.packetevents.protocol.item.enchantment.type.EnchantmentTypes;
 import org.bukkit.Bukkit;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Entity;
@@ -106,30 +105,7 @@ public class VirtualDisplayItem<T> extends AbstractDisplayItem implements Reload
       cloned.getEnchantments().clear();
       return cloned;
     }
-
-    final int amount = cloned.getEnchantments().size();
-    boolean removed = false;
-
-    for(final Map.Entry<Enchantment, Integer> entry : itemStack.getEnchantments().entrySet()) {
-
-      final Enchantment enchantment = entry.getKey();
-
-      if(EnchantmentTypes.getRegistry().getByName(enchantment.key().asString()) == null) {
-        cloned.removeEnchantment(enchantment);
-        removed = true;
-        continue;
-      }
-      if(EnchantmentTypes.getByName(enchantment.key().asString()) == null) {
-        cloned.removeEnchantment(enchantment);
-        removed = true;
-      }
-    }
-
-    if(amount == 1 && removed) {
-      cloned.addUnsafeEnchantment(Enchantment.UNBREAKING, 1);
-    }
-
-    return cloned;
+    return (manager.packetHandler() != null)? manager.packetHandler().filterEnchantments(cloned) : cloned;
   }
 
   @Override
