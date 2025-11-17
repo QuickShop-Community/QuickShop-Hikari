84befb53f5ec3ac9f3b14c1defe6079d62ec5ca5
Daniel V. <daniel.viddy@gmail.com>
2025-11-16 21:04:01 -0500

    Add compatibility for CrazyCrates, ExcellentCrates, and SilkSpawners in MatcherPlus module.
    - Integrated support for CrazyCrates, ExcellentCrates, and SilkSpawners plugins.
    - Updated dependencies and repositories in `pom.xml`.
    - Registered new checks in `Main` class for enhanced compatibility.
    - Adjusted `plugin.yml` for proper loading with soft dependencies.
    - Updated changelog with compatibility improvements.

--- compatibility/matcherplus/src/main/java/com/ghostchu/quickshop/compatibility/matcherplus/matchers/impl/ExcellentCratesCheck.java
+++ compatibility/matcherplus/src/main/java/com/ghostchu/quickshop/compatibility/matcherplus/matchers/impl/ExcellentCratesCheck.java
@@ -1,4 +1,52 @@
 package com.ghostchu.quickshop.compatibility.matcherplus.matchers.impl;
 
-public class ExcellentCratesCheck {
+import com.badbones69.crazycrates.paper.api.enums.other.keys.ItemKeys;
+import com.ghostchu.quickshop.compatibility.matcherplus.matchers.ItemCheck;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.persistence.PersistentDataType;
+import org.jetbrains.annotations.Nullable;
+import su.nightexpress.excellentcrates.CratesAPI;
+import su.nightexpress.excellentcrates.key.CrateKey;
+
+public class ExcellentCratesCheck implements ItemCheck {
+
+  /**
+   * Check if this check applies to the specified ItemStack
+   *
+   * @param stack the ItemStack to check
+   *
+   * @return true if the check applies to the ItemStack, otherwise false
+   */
+  @Override
+  public boolean applies(final @Nullable ItemStack stack) {
+
+    return stack != null && CratesAPI.getKeyManager().isKey(stack);
+  }
+
+  /**
+   * Checks if two ItemStack objects match each other.
+   *
+   * @param stack   the first ItemStack to compare
+   * @param compare the second ItemStack to compare
+   *
+   * @return true if the two ItemStack objects match, false otherwise
+   */
+  @Override
+  public boolean matches(final @Nullable ItemStack stack, final @Nullable ItemStack compare) {
+
+    final CrateKey originalKey = (stack != null)? CratesAPI.getKeyManager().getKeyByItem(stack) : null;
+    final CrateKey compareKey = (compare != null)? CratesAPI.getKeyManager().getKeyByItem(compare) : null;
+
+    if(originalKey == null) {
+
+      return compareKey == null;
+    }
+
+    if(compareKey == null) {
+
+      return false;
+    }
+
+    return originalKey.getId().equals(compareKey.getId());
+  }
 }
--- compatibility/matcherplus/src/main/java/com/ghostchu/quickshop/compatibility/matcherplus/matchers/impl/SilkSpawnerCheck.java
+++ compatibility/matcherplus/src/main/java/com/ghostchu/quickshop/compatibility/matcherplus/matchers/impl/SilkSpawnerCheck.java
@@ -1,4 +1,79 @@
 package com.ghostchu.quickshop.compatibility.matcherplus.matchers.impl;
 
-public class SilkSpawnerCheck {
+import com.badbones69.crazycrates.paper.api.enums.other.keys.ItemKeys;
+import com.ghostchu.quickshop.compatibility.matcherplus.matchers.ItemCheck;
+import de.dustplanet.util.SilkUtil;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.persistence.PersistentDataType;
+import org.jetbrains.annotations.Nullable;
+
+public class SilkSpawnerCheck implements ItemCheck {
+
+  private final String defaultValue = "no-entity";
+  private final SilkUtil util;
+
+  public SilkSpawnerCheck() {
+
+    this.util = SilkUtil.hookIntoSilkSpanwers();
+  }
+
+  /**
+   * Check if this check applies to the specified ItemStack
+   *
+   * @param stack the ItemStack to check
+   *
+   * @return true if the check applies to the ItemStack, otherwise false
+   */
+  @Override
+  public boolean applies(final @Nullable ItemStack stack) {
+
+    if(stack == null) {
+      return false;
+    }
+
+    return util.getStoredEggEntityID(stack) != null || util.getStoredSpawnerItemEntityID(stack) != null;
+  }
+
+  /**
+   * Checks if two ItemStack objects match each other.
+   *
+   * @param stack   the first ItemStack to compare
+   * @param compare the second ItemStack to compare
+   *
+   * @return true if the two ItemStack objects match, false otherwise
+   */
+  @Override
+  public boolean matches(final @Nullable ItemStack stack, final @Nullable ItemStack compare) {
+
+    final String originalCreature = creatureString(stack);
+    final String compareCreature = creatureString(compare);
+
+    if(originalCreature == null) {
+
+      return compareCreature == null;
+    }
+
+    if(compareCreature == null) {
+
+      return false;
+    }
+
+    return originalCreature.equals(compareCreature);
+  }
+
+  @Nullable
+  public String creatureString(final @Nullable ItemStack stack) {
+
+    if(stack == null) {
+
+      return null;
+    }
+
+    final String entityID = util.getStoredEggEntityID(stack);
+    if(entityID != null) {
+      return entityID;
+    }
+
+    return util.getStoredSpawnerItemEntityID(stack);
+  }
 }
