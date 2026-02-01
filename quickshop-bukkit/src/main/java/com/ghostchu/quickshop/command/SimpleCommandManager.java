bfd58a39989f4956f1cdec70ebb0c979e78ac806
MrlingXD <90316914+wling-art@users.noreply.github.com>
2026-02-02 03:23:12 +0800

    Fix command description language key mismatch

--- quickshop-bukkit/src/main/java/com/ghostchu/quickshop/command/SimpleCommandManager.java
+++ quickshop-bukkit/src/main/java/com/ghostchu/quickshop/command/SimpleCommandManager.java
@@ -678,12 +678,11 @@ public class SimpleCommandManager implements CommandManager, TabCompleter, Comma
     cmds.removeIf(commandContainer->commandContainer.getPrefix().equalsIgnoreCase(container.getPrefix()));
     cmds.removeIf(container::equals);
 
-    final String oldPrefix = container.getPrefix();
-    final String newPrefix = plugin.getCommandPrefix(oldPrefix);
+    final String newPrefix = plugin.getCommandPrefix(container.getPrefix());
     if(newPrefix != null && !newPrefix.isEmpty()) {
 
       container.setPrefix(newPrefix);
-      container.setDescription((locale)->plugin.text().of("command.description." + oldPrefix).forLocale());
+      container.setDescription((locale)->plugin.text().of("command.description." + newPrefix).forLocale());
     }
     cmds.add(container);
     cmds.sort(Comparator.comparing(CommandContainer::getPrefix));
