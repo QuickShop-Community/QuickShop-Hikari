e8716657ff0e0a8e9a4e6cfaa02b09e8f42605fe
Daniel V. <daniel.viddy@gmail.com>
2026-06-08 13:54:38 -0400

    Readd research variables.

--- quickshop-bukkit/src/main/java/com/ghostchu/quickshop/util/metric/faststats/FastStatsCollector.java
+++ quickshop-bukkit/src/main/java/com/ghostchu/quickshop/util/metric/faststats/FastStatsCollector.java
@@ -21,6 +21,7 @@ package com.ghostchu.quickshop.util.metric.faststats;
 import com.ghostchu.quickshop.util.metric.MetricCollectEntry;
 import com.ghostchu.quickshop.util.metric.MetricDataType;
 import com.ghostchu.quickshop.util.metric.StatCollector;
+import dev.faststats.core.data.Metric;
 import org.bstats.charts.AdvancedPie;
 import org.bstats.charts.CustomChart;
 import org.bstats.charts.DrilldownPie;
@@ -40,5 +41,59 @@ public class FastStatsCollector {
   //TODO: Fully implement this class for research statistics. Will be implemented in future release.
   public FastStatsCollector(final StatCollector collector) {
     this.collector = collector;
+  }
+
+  @MetricCollectEntry(dataType = MetricDataType.RESEARCH, moduleName = "Statistic - All shops hosted across all servers", description = "How many shops we can power across all servers? This research will used for performance tweak for components like shop managing/looking up/caching size etc.")
+  public Metric<Number> statisticAllShops() {
+
+    return Metric.number("statistic_all_shops", collector::statisticAllShops);
+  }
+
+  @MetricCollectEntry(dataType = MetricDataType.RESEARCH, moduleName = "Statistic - All tags hosted across all servers", description = "How many tags we power across all servers? This research will used for performance tweak for components like tag managing/looking up/caching size etc.")
+  public Metric<Number> statisticAllTags() {
+
+    return Metric.number("statistic_all_tags", collector::statisticAllTags);
+  }
+
+  @MetricCollectEntry(dataType = MetricDataType.RESEARCH, moduleName = "Research - Item Stacking Shop", description = "We collect this for determine if we should push Item Stacking Shop to a feature that default enabled.")
+  public Metric<Boolean> researchItemStackingShop() {
+
+    return Metric.bool("statistic_stacking", collector::researchItemStackingShop);
+  }
+
+  @MetricCollectEntry(dataType = MetricDataType.STATISTIC, moduleName = "Statistic - Database Types", description = "We collect this so we can know the percent of different database types users.")
+  public Metric<String> statisticDatabaseTypes() {
+
+    return Metric.string("statistic_database_types", collector::statisticDatabaseType);
+  }
+
+  @MetricCollectEntry(dataType = MetricDataType.STATISTIC, moduleName = "Statistic - Economy Types", description = "We collect this so we can know the percent of different economy types users.")
+  public Metric<String> statisticEconomyTypes() {
+
+    return Metric.string("statistic_economy_types", collector::statisticEconomyType);
+  }
+
+  @MetricCollectEntry(dataType = MetricDataType.STATISTIC, moduleName = "Statistic - ItemMatcher", description = "We collect this so we can know the item matcher that users using, and improve it.")
+  public Metric<String> statisticItemMatcher() {
+
+    return Metric.string("statistic_item_matcher", collector::statisticItemMatcher);
+  }
+
+  @MetricCollectEntry(dataType = MetricDataType.STATISTIC, moduleName = "Statistic - Publisher", description = "We count the name of the publisher (in BuildInfo) so that we know if someone else is recompiling our plugin without changing the fork name. if you are a QuickShop-Hikari fork developer, please change the return value of your getFork() to something else in order to separate it from the stats. This value is usually fixed to Ghost-chu@Hikari.")
+  public Metric<String> statisticPublisher() {
+
+    return Metric.string("statistic_publisher", collector::statisticPublisher);
+  }
+
+  @MetricCollectEntry(dataType = MetricDataType.RESEARCH, moduleName = "Research - ProtocolLib Version", description = "We collect this so we can know the which one ProtocolLib is popular. ProtocolLib sometimes releases destructive updates, so we collect this metric to know the distribution of ProtocolLib versions among users and remove unused ProtocolLib workaround code to improve code maintainability and program performance.")
+  public Metric<String> researchProtocolLibVersion() {
+
+    return Metric.string("statistic_protocollib", collector::researchProtocolLibVersion);
+  }
+
+  @MetricCollectEntry(dataType = MetricDataType.RESEARCH, moduleName = "Research - PacketEvents Version", description = "We collect this so we can know the which one PacketEvents is popular. PacketEvents sometimes releases destructive updates, so we collect this metric to know the distribution of PacketEvents versions among users and remove unused PacketEvents workaround code to improve code maintainability and program performance.")
+  public Metric<String> researchPacketEventsVersion() {
+
+    return Metric.string("statistic_packetvents", collector::researchPacketEventsVersion);
   }
 }
