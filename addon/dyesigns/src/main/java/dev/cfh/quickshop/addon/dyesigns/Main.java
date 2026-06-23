cf298687c0f97ba1453c8bed617311a46b1fb46b
creatorfromhell <daniel.viddy@gmail.com>
2026-06-23 07:40:30 -0400

    Add license header and since tags

--- addon/dyesigns/src/main/java/dev/cfh/quickshop/addon/dyesigns/Main.java
+++ addon/dyesigns/src/main/java/dev/cfh/quickshop/addon/dyesigns/Main.java
@@ -1,5 +1,23 @@
 package dev.cfh.quickshop.addon.dyesigns;
 
+/*
+ * QuickShop-Hikari
+ * Copyright (C) 2026 Daniel "creatorfromhell" Vidmar
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import com.ghostchu.quickshop.QuickShop;
 import com.ghostchu.quickshop.api.event.general.ShopSignUpdateEvent;
 import com.ghostchu.quickshop.api.shop.Shop;
@@ -28,6 +46,10 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.function.BiConsumer;
 
+/**
+ * @author creatorfromhell
+ * @since 6.3.0.0
+ */
 public final class Main extends JavaPlugin implements Listener {
 
   private NamespacedKey DYE_NAMESPACE_KEY;
