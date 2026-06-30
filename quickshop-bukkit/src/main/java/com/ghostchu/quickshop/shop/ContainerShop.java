4c1536b66ad8807bbc677695ce75e6a5450b1462
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2026-07-01 00:36:54 +0200

    Fix shop preview locales being sticky

--- quickshop-bukkit/src/main/java/com/ghostchu/quickshop/shop/ContainerShop.java
+++ quickshop-bukkit/src/main/java/com/ghostchu/quickshop/shop/ContainerShop.java
@@ -135,8 +135,6 @@ public class ContainerShop implements Shop<Double, Location>, Reloadable {
   @EqualsAndHashCode.Exclude
   private volatile boolean createBackup = false;
   @EqualsAndHashCode.Exclude
-  private InventoryPreview inventoryPreview = null;
-  @EqualsAndHashCode.Exclude
   private boolean dirty;
   @EqualsAndHashCode.Exclude
   private boolean updating = false;
@@ -528,11 +526,6 @@ public class ContainerShop implements Shop<Double, Location>, Reloadable {
     }
     this.displayItem = null;
     checkDisplay();
-    if(this.inventoryPreview != null) {
-
-      this.inventoryPreview.close();
-      this.inventoryPreview = null;
-    }
     setSignText();
     setDirty();
   }
@@ -1562,9 +1555,6 @@ public class ContainerShop implements Shop<Double, Location>, Reloadable {
       Log.debug("Dupe unload request, canceled.");
       return;
     }
-    if(inventoryPreview != null) {
-      inventoryPreview.close();
-    }
     if(this.displayItem != null) {
       this.displayItem.remove(dontTouchWorld);
     }
@@ -1576,10 +1566,7 @@ public class ContainerShop implements Shop<Double, Location>, Reloadable {
   @Override
   public void openPreview(@NotNull final Player player) {
 
-    if(inventoryPreview == null) {
-      inventoryPreview = new InventoryPreview(plugin, getItem().clone(), player.getLocale());
-    }
-    inventoryPreview.show(player);
+    InventoryPreview.show(player, getItem());
 
   }
 
@@ -2139,7 +2126,6 @@ public class ContainerShop implements Shop<Double, Location>, Reloadable {
            ", displayItem=" + displayItem +
            ", isLoaded=" + isLoaded +
            ", createBackup=" + createBackup +
-           ", inventoryPreview=" + inventoryPreview +
            ", dirty=" + dirty +
            ", updating=" + updating +
            ", currency='" + currency + '\'' +
