00f7d602295a9a539ee9192d85b8f077f3896361
YuanYuanOwO <81153017+YuanYuanOwO@users.noreply.github.com>
2026-08-16 21:27:54 +0800

    Fixed an issue where including legacy color codes in item components could cause errors (#2493)
    * Fix legacy color issue
    
    * Fix build

--- quickshop-bukkit/src/main/java/com/ghostchu/quickshop/localization/text/SimpleTextManager.java
+++ quickshop-bukkit/src/main/java/com/ghostchu/quickshop/localization/text/SimpleTextManager.java
@@ -852,8 +852,7 @@ public class SimpleTextManager implements TextManager, Reloadable, SubPasteItem 
           return Collections.singletonList(LegacyComponentSerializer.legacySection().deserialize(path));
         }
         final List<Component> components = str.stream()
-                .map(s -> MiniMessageFiller.fillRaw(s, args))
-                .map(s -> manager.plugin.platform().miniMessage().deserialize(s, tagResolvers))
+                .map(s -> MiniMessageFiller.fill(s, manager.plugin.platform().miniMessage(), tagResolvers, args))
                 .toList();
         return postProcess(components);
       }
@@ -1016,9 +1015,7 @@ public class SimpleTextManager implements TextManager, Reloadable, SubPasteItem 
           return LegacyComponentSerializer.legacySection().deserialize(path);
         }
 
-        final String filled = MiniMessageFiller.fillRaw(str, args);
-        final Component component = manager.plugin.platform().miniMessage().deserialize(filled, tagResolvers);
-        //final Component component = MiniMessageFiller.fill(str, manager.plugin.platform().miniMessage(), tagResolvers, args);
+        final Component component = MiniMessageFiller.fill(str, manager.plugin.platform().miniMessage(), tagResolvers, args);
         return postProcess(component);
       }
     }
